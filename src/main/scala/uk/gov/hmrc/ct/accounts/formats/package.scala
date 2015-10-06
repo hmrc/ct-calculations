f73ceb3d9d7246d9b6a84043715540b3ef6413ce
Keith Hall <keith@flyingkitesconsulting.com>
2015-10-06 14:47:27 +0100

    CATO-2863:  Add box AC12 for turnover

--- src/main/scala/uk/gov/hmrc/ct/accounts/formats/package.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/formats/package.scala
@@ -16,14 +16,16 @@
 
 package uk.gov.hmrc.ct.accounts
 
-import play.api.libs.json.{Format, Json}
-import uk.gov.hmrc.ct.box.formats.{OptionalStringFormat, OptionalDateFormat, DateFormat}
+import play.api.libs.json.Format
+import uk.gov.hmrc.ct.box.formats.{OptionalIntegerFormat, OptionalStringFormat, OptionalDateFormat, DateFormat}
 
 package object formats {
   implicit val ac1Format: Format[AC1] = new OptionalStringFormat[AC1](AC1.apply)
 
   implicit val ac3Format: Format[AC3] = new DateFormat[AC3](AC3.apply)
   implicit val ac4Format: Format[AC4] = new DateFormat[AC4](AC4.apply)
+
+  implicit val ac12Format: Format[AC12] = new OptionalIntegerFormat[AC12](AC12.apply)
 
   implicit val ac205Format: Format[AC205] = new OptionalDateFormat[AC205](AC205.apply)
   implicit val ac206Format: Format[AC206] = new OptionalDateFormat[AC206](AC206.apply)
