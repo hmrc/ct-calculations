ab731e21297d0e9f5b42e9a61d95fbfe333bfcf2
James Williams <james@liquid-armour.co.uk>
2015-10-19 11:11:33 +0100

    CATO-2721 Removed CP503 from total additions calculation.

--- src/test/scala/uk/gov/hmrc/ct/computations/calculations/TotalAdditionsCalculatorSpec.scala
+++ src/test/scala/uk/gov/hmrc/ct/computations/calculations/TotalAdditionsCalculatorSpec.scala
@@ -22,14 +22,13 @@ import uk.gov.hmrc.ct.computations._
 class TotalAdditionsCalculatorSpec extends WordSpec with Matchers {
   "Total Additions Calculator" should {
     "calculate additions with populated values" in new TotalAdditionsCalculator {
-      totalAdditionsCalculation(cp503 = CP503(Some(1)),
-                                cp46 = CP46(Some(2)),
+      totalAdditionsCalculation(cp46 = CP46(Some(2)),
                                 cp47 = CP47(Some(3)),
                                 cp48 = CP48(Some(4)),
                                 cp49 = CP49(Some(5)),
                                 cp51 = CP51(Some(6)),
                                 cp52 = CP52(Some(7)),
-                                cp53 = CP53(Some(8))) shouldBe CP54(36)
+                                cp53 = CP53(Some(8))) shouldBe CP54(35)
     }
   }
 }
