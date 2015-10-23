02dbd3e697426813fc7f421f05fc678c71742821
adam.hudspith <adam.hudspith@digital.hmrc.gov.uk>
2015-10-23 15:14:00 +0100

    CATO-2552 repayments v3 modifications - added unit tests for B860

---
+++ src/test/scala/uk/gov/hmrc/ct/ct600/v3/B860Spec.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2015 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ct.ct600.v3
+
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{Matchers, WordSpec}
+import uk.gov.hmrc.ct.box.CtValidation
+import uk.gov.hmrc.ct.ct600.v3.retriever.CT600BoxRetriever
+
+
+class B860Spec extends WordSpec with MockitoSugar with Matchers {
+
+  "B860 validate" should {
+    "not return errors when REPAYMENTSQ1 is true" in {
+      val mockBoxRetriever = mock[CT600BoxRetriever]
+      when(mockBoxRetriever.retrieveREPAYMENTSQ1()).thenReturn(REPAYMENTSQ1(Some(true)))
+
+      B860(None).validate(mockBoxRetriever) shouldBe Set()
+    }
+
+    "not return errors when REPAYMENTSQ1 is false and B860 is valid" in {
+      val mockBoxRetriever = mock[CT600BoxRetriever]
+      when(mockBoxRetriever.retrieveREPAYMENTSQ1()).thenReturn(REPAYMENTSQ1(Some(false)))
+
+      B860(Some(100)).validate(mockBoxRetriever) shouldBe Set()
+    }
+
+    "return error when REPAYMENTSQ1 is false and B860 is blank" in {
+      val mockBoxRetriever = mock[CT600BoxRetriever]
+      when(mockBoxRetriever.retrieveREPAYMENTSQ1()).thenReturn(REPAYMENTSQ1(Some(false)))
+
+      B860(None).validate(mockBoxRetriever) shouldBe Set(CtValidation(Some("B860"), "error.B860.required", None))
+    }
+
+    "return error when REPAYMENTSQ1 is false and B860 is negative" in {
+      val mockBoxRetriever = mock[CT600BoxRetriever]
+      when(mockBoxRetriever.retrieveREPAYMENTSQ1()).thenReturn(REPAYMENTSQ1(Some(false)))
+
+      B860(Some(-1)).validate(mockBoxRetriever) shouldBe Set(CtValidation(Some("B860"), "error.B860.mustBePositive", None))
+    }
+  }
+}
