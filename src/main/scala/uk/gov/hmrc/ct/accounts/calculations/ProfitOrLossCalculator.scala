de27f96870c034003853284f259e391ddd13d244
James Williams <james@liquid-armour.co.uk>
2015-11-12 13:00:51 +0100

    CATO-3006 Wrote tests and code for Previous and Current Gross profit or loss.

--- src/main/scala/uk/gov/hmrc/ct/accounts/calculations/ProfitOrLossCalculator.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/calculations/ProfitOrLossCalculator.scala
@@ -14,10 +14,25 @@ trait ProfitOrLossCalculator {
   }
 
   def calculateCurrentGrossProfitOrLoss(ac12: AC12, ac14: AC14, statutoryAccountsFiling: StatutoryAccountsFiling): AC16 = {
-    AC16(None)
+    AC16(if (statutoryAccountsFiling.value) {
+      calculateProfitOrLoss(ac12.value, ac14.value)
+    }
+    else None)
   }
 
   def calculatePreviousGrossProfitOrLoss(ac13: AC13, ac15: AC15, statutoryAccountsFiling: StatutoryAccountsFiling): AC17 = {
-    AC17(None)
+    AC17 (
+      if (statutoryAccountsFiling.value) {
+        calculateProfitOrLoss(ac13.value, ac15.value)
+      }
+      else None
+    )
+  }
+
+  private def calculateProfitOrLoss(turnover: Option[Int], costs: Option[Int]): Option[Int] = {
+    (turnover, costs) match {
+      case (None, None) => None
+      case (t, c) => Some(t.getOrElse(0) - c.getOrElse(0))
+    }
   }
 }
--- src/test/scala/uk/gov/hmrc/ct/accounts/calculations/ProfitOrLossCalculatorSpec.scala
+++ src/test/scala/uk/gov/hmrc/ct/accounts/calculations/ProfitOrLossCalculatorSpec.scala
@@ -12,11 +12,35 @@ class ProfitOrLossCalculatorSpec extends WordSpec with Matchers with ProfitOrLos
       "return None if not statutory accounts" in {
         calculatePreviousGrossProfitOrLoss(ac13 = AC13(Some(100)), ac15 = AC15(None), StatutoryAccountsFiling(false)) shouldBe AC17(None)
       }
+      "return None if statutory accounts and cp13 and cp15 are None" in {
+        calculatePreviousGrossProfitOrLoss(ac13 = AC13(None), ac15 = AC15(None), StatutoryAccountsFiling(true)) shouldBe AC17(None)
+      }
+      "return negative value of CP15 if statutory accounts and cp13 is None and cp15 has a value" in {
+        calculatePreviousGrossProfitOrLoss(ac13 = AC13(None), ac15 = AC15(Some(123)), StatutoryAccountsFiling(true)) shouldBe AC17(Some(-123))
+      }
+      "return value of CP13 if statutory accounts and cp15 is None and cp13 has a value" in {
+        calculatePreviousGrossProfitOrLoss(ac13 = AC13(Some(321)), ac15 = AC15(None), StatutoryAccountsFiling(true)) shouldBe AC17(Some(321))
+      }
+      "return value of CP13 - CP15 if statutory accounts and cp15 and cp13 have values" in {
+        calculatePreviousGrossProfitOrLoss(ac13 = AC13(Some(321)), ac15 = AC15(Some(300)), StatutoryAccountsFiling(true)) shouldBe AC17(Some(21))
+      }
     }
 
     "calculateCurrentGrossProfitOrLoss" when {
       "return None if not statutory accounts" in {
         calculateCurrentGrossProfitOrLoss(ac12 = AC12(Some(100)), ac14 = AC14(None), StatutoryAccountsFiling(false)) shouldBe AC16(None)
+      }
+      "return None if statutory accounts and cp12 and cp14 are None" in {
+        calculateCurrentGrossProfitOrLoss(ac12 = AC12(None), ac14 = AC14(None), StatutoryAccountsFiling(true)) shouldBe AC16(None)
+      }
+      "return negative value of CP15 if statutory accounts and cp13 is None and cp15 has a value" in {
+        calculateCurrentGrossProfitOrLoss(ac12 = AC12(None), ac14 = AC14(Some(123)), StatutoryAccountsFiling(true)) shouldBe AC16(Some(-123))
+      }
+      "return value of CP13 if statutory accounts and cp15 is None and cp13 has a value" in {
+        calculateCurrentGrossProfitOrLoss(ac12 = AC12(Some(321)), ac14 = AC14(None), StatutoryAccountsFiling(true)) shouldBe AC16(Some(321))
+      }
+      "return value of CP13 - CP15 if statutory accounts and cp15 and cp13 have values" in {
+        calculateCurrentGrossProfitOrLoss(ac12 = AC12(Some(321)), ac14 = AC14(Some(21)), StatutoryAccountsFiling(true)) shouldBe AC16(Some(300))
       }
     }
 
