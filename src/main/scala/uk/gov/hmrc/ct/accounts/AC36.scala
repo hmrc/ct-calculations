1b37cb982cdfe507e6495827fccebe2c877bb21c
James Williams <james@liquid-armour.co.uk>
2015-11-16 09:10:21 +0000

    CATO-3006 added formatters and more calculations

--- src/main/scala/uk/gov/hmrc/ct/accounts/AC36.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/AC36.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.ct.accounts
 
 import uk.gov.hmrc.ct.accounts.retriever.AccountsBoxRetriever
@@ -8,5 +24,3 @@ case class AC36(value: Option[Int]) extends CtBoxIdentifier(name = "Current Prof
 object AC36 extends Calculated[AC36, AccountsBoxRetriever] {
   override def calculate(boxRetriever: AccountsBoxRetriever): AC36 = ???
 }
-
-
