b821b064d38c608cb5eb0e1a6ac468cdd8383339
paul.campbell <paul.campbell@digital.hmrc.gov.uk>
2015-11-25 12:10:26 +0000

    CATO-2431 add B45Input #2

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/B45Input.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/B45Input.scala
@@ -23,6 +23,6 @@ import uk.gov.hmrc.ct.ct600.v3.retriever.CT600BoxRetriever
 case class B45Input(value: Option[Boolean]) extends CtBoxIdentifier("Are you owed a repayment for an earlier period?")
   with CtOptionalBoolean with Input with ValidatableBox[CT600BoxRetriever] {
 
-  def validate(boxRetriever: CT600BoxRetriever): Set[CtValidation] = validateBooleanAsMandatory("B45", this)
+  def validate(boxRetriever: CT600BoxRetriever): Set[CtValidation] = validateBooleanAsMandatory("B45Input", this)
 
 }
