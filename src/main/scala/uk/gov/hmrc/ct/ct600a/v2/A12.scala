0ddd6dadff5c820a36f14a6f39d8cf3a0d32b1a5
paul.campbell <paul.campbell@digital.hmrc.gov.uk>
2015-12-02 14:28:58 +0000

    CATO-3088 fix loans outstanding total

--- src/main/scala/uk/gov/hmrc/ct/ct600a/v2/A12.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600a/v2/A12.scala
@@ -26,7 +26,6 @@ case class A12(value: Option[Int]) extends CtBoxIdentifier(name = "A12 - Total o
 object A12 extends Calculated[A12, CT600ABoxRetriever] with LoansToParticipatorsCalculator {
 
   override def calculate(fieldValueRetriever: CT600ABoxRetriever): A12 = {
-    calculateA12(fieldValueRetriever.retrieveA2(), fieldValueRetriever.retrieveLP04(),
-                 fieldValueRetriever.retrieveA6(), fieldValueRetriever.retrieveA10())
+    calculateA12(fieldValueRetriever.retrieveA2(), fieldValueRetriever.retrieveLP04())
   }
 }
