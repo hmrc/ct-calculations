70048479363a33dba31980a9fe3c155dbc8f4a59
paul.campbell <paul.campbell@digital.hmrc.gov.uk>
2015-12-11 08:57:42 +0000

    CATO-2416 link B480 to A80

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/B480.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/B480.scala
@@ -17,16 +17,17 @@
 package uk.gov.hmrc.ct.ct600.v3
 
 import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.computations.CP265
+import uk.gov.hmrc.ct.ct600.v3.B235
 import uk.gov.hmrc.ct.ct600.v3.calculations.CorporationTaxCalculator
 import uk.gov.hmrc.ct.ct600.v3.retriever.CT600BoxRetriever
+import uk.gov.hmrc.ct.ct600a.v3.A80
 
 
 case class B480(value: Option[BigDecimal]) extends CtBoxIdentifier(name = "Tax payable under S419 ICTA 1988") with AnnualConstant with CtOptionalBigDecimal
 
-object B480 extends CorporationTaxCalculator with Calculated[B480, CT600BoxRetriever] {
+object B480 extends Linked[A80, B235] {
 
-  override def calculate(fieldValueRetriever: CT600BoxRetriever): B480 = {
-    // TODO: replace with real calculation once loans to participators stuff is implemented
-    B480(None)
-  }
+  override def apply(source: A80): B480 = B480(source.value)
 }
+
