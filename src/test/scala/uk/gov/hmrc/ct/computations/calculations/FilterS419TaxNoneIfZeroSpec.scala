aba9ea2993c7b29246862a8e35bc8104f748d260
paul.campbell <paul.campbell@digital.hmrc.gov.uk>
2015-12-11 14:57:54 +0000

    CATO-2416 B480 none if A80 0

---
+++ src/test/scala/uk/gov/hmrc/ct/computations/calculations/FilterS419TaxNoneIfZeroSpec.scala
@@ -0,0 +1,20 @@
+package uk.gov.hmrc.ct.computations.calculations
+
+import org.scalatest.{Matchers, WordSpec}
+import uk.gov.hmrc.ct.ct600.v3.B480
+import uk.gov.hmrc.ct.ct600a.v3.A80
+
+
+class FilterS419TaxNoneIfZeroSpec extends WordSpec with Matchers {
+
+  "B480" should {
+    "be A80 value if A80 != 0" in new FilterS419TaxNoneIfZero {
+      val a80 = A80(Some(95))
+      filterZero(a80) shouldBe B480(Some(95))
+    }
+    "be non value if A80 == 0" in new FilterS419TaxNoneIfZero {
+      val a80 = A80(Some(0))
+      filterZero(a80) shouldBe B480(None)
+    }
+  }
+}
