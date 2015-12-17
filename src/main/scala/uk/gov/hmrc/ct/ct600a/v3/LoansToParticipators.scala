526e60d556502bf8126a5bc8926ab4705a8bdadd
scott.cutts <scott.cutts@digital.hmrc.gov.uk>
2015-12-17 15:22:13 +0000

    CATO-2985: Added validation for Loan Write Offs

--- src/main/scala/uk/gov/hmrc/ct/ct600a/v3/LoansToParticipators.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600a/v3/LoansToParticipators.scala
@@ -18,8 +18,10 @@ package uk.gov.hmrc.ct.ct600a.v3
 
 import org.joda.time.LocalDate
 import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.computations.CP2
 import uk.gov.hmrc.ct.ct600.v3.retriever.CT600BoxRetriever
 import uk.gov.hmrc.ct.ct600a.v3.formats.LoansFormatter
+import uk.gov.hmrc.ct.domain.ValidationConstants._
 
 
 case class LoansToParticipators(loans: List[Loan] = List.empty) extends CtBoxIdentifier(name = "Loans to participators.") with CtValue[List[Loan]] with Input with ValidatableBox[CT600BoxRetriever] {
@@ -30,29 +32,8 @@ case class LoansToParticipators(loans: List[Loan] = List.empty) extends CtBoxIde
 
   override def asBoxString = LoansFormatter.asBoxString(this)
 
-  override def validate(boxRetriever: CT600BoxRetriever): Set[CtValidation] = {
-    validateLoans(invalidLoanNameLength, "error.loan.name.length") ++
-    validateLoans(invalidLoanAmount, "error.loan.amount.value") ++
-    validateLoans(invalidRepayedWithin9Months, "error.loan.isRepaidWithin9Months.required") ++
-    validateLoans(invalidRepayedAfter9Months, "error.loan.isRepaidAfter9Months.required") ++
-    validateLoans(invalidHasWriteOffs, "error.loan.hasWriteOffs.required")
-  }
-
-  private def invalidLoanNameLength(loan: Loan): Boolean = loan.name.length < 2 || loan.name.length > 56
-
-  private def invalidLoanAmount(loan: Loan): Boolean = loan.amount < 1 || loan.amount > 99999999
-
-  private def invalidRepayedWithin9Months(loan: Loan): Boolean = loan.isRepaidWithin9Months.isEmpty
-
-  private def invalidRepayedAfter9Months(loan: Loan): Boolean = loan.isRepaidAfter9Months.isEmpty
-
-  private def invalidHasWriteOffs(loan: Loan): Boolean = loan.hasWriteOffs.isEmpty
-
-  def validateLoans(invalid: Loan => Boolean, errorMsg: String): Set[CtValidation] = {
-    loans.filter(invalid).map { loan =>
-        CtValidation(Some(s"LoansToParticipators"), s"loan.${loan.id}.$errorMsg", None)
-    }.toSet
-  }
+  override def validate(boxRetriever: CT600BoxRetriever): Set[CtValidation] =
+    loans.foldRight(Set[CtValidation]())((loan, tail) => loan.validate(boxRetriever) ++ tail)
 }
 
 case class Loan ( id: String,
@@ -63,11 +44,59 @@ case class Loan ( id: String,
                   isRepaidAfter9Months: Option[Boolean] = None,
                   otherRepayments: List[Repayment] = List.empty,
                   hasWriteOffs: Option[Boolean] = None,
-                  writeOffs: List[WriteOff] = List.empty)
+                  writeOffs: List[WriteOff] = List.empty) {
+
+  def validate(boxRetriever: CT600BoxRetriever): Set[CtValidation] = {
+    validateLoan(invalidLoanNameLength, "error.loan.name.length") ++
+    validateLoan(invalidLoanAmount, "error.loan.amount.value") ++
+    validateLoan(invalidRepayedWithin9Months, "error.loan.isRepaidWithin9Months.required") ++
+    validateLoan(invalidRepayedAfter9Months, "error.loan.isRepaidAfter9Months.required") ++
+    validateLoan(invalidHasWriteOffs, "error.loan.hasWriteOffs.required") ++
+    writeOffs.foldRight(Set[CtValidation]())((writeOff, tail) => writeOff.validate(boxRetriever, id) ++ tail)
+  }
+
+  private def invalidLoanNameLength: Boolean = name.length < 2 || name.length > 56
+
+  private def invalidLoanAmount: Boolean = amount < MIN_MONEY_AMOUNT_ALLOWED || amount > MAX_MONEY_AMOUNT_ALLOWED
+
+  private def invalidRepayedWithin9Months: Boolean = isRepaidWithin9Months.isEmpty
+
+  private def invalidRepayedAfter9Months: Boolean = isRepaidAfter9Months.isEmpty
+
+  private def invalidHasWriteOffs: Boolean = hasWriteOffs.isEmpty
+
+  def validateLoan(invalid: Boolean, errorMsg: String): Set[CtValidation] = {
+   invalid match {
+     case true => Set(CtValidation(Some(s"LoansToParticipators"), s"loan.$id.$errorMsg", None))
+     case false => Set.empty
+    }
+  }
+
+}
 
 case class Repayment(id: String, amount: Int, date: LocalDate, endDateOfAP: Option[LocalDate] = None) extends LoansDateRules
 
-case class WriteOff(id: String, amount: Int, date: LocalDate, endDateOfAP : Option[LocalDate] = None) extends LoansDateRules
+case class WriteOff(id: String, amount: Int, date: LocalDate, endDateOfAP : Option[LocalDate] = None) extends LoansDateRules {
+
+  def validate(boxRetriever: CT600BoxRetriever, loanId: String): Set[CtValidation] = {
+    validateWriteOff(invalidDate(boxRetriever.retrieveCP2()), "error.writeOff.date.range", loanId) ++
+    validateWriteOff(invalidWriteOffAmount, "error.writeOff.amount.value", loanId) ++
+    validateWriteOff(invalidApEndDate(boxRetriever.retrieveCP2()), "error.writeOff.apEndDate.range", loanId)
+  }
+
+  private def invalidDate(cp2: CP2): Boolean = !(date.isAfter(cp2.value) && date.isBefore(LocalDate.now().plusDays(1).toDateTimeAtStartOfDay.toLocalDate))
+
+  private def invalidWriteOffAmount: Boolean = amount < MIN_MONEY_AMOUNT_ALLOWED || amount > MAX_MONEY_AMOUNT_ALLOWED
+
+  private def invalidApEndDate(cp2: CP2): Boolean = !endDateOfAP.map(_.isAfter(cp2.value)).getOrElse(true)
+
+  def validateWriteOff(invalid: Boolean, errorMsg: String, loanId: String): Set[CtValidation] = {
+    invalid match {
+      case true => Set(CtValidation(Some(s"LoansToParticipators"), s"loan.$loanId.writeOff.$id.$errorMsg", None))
+      case false => Set.empty
+    }
+  }
+}
 
 
 trait LoansDateRules {
--- src/test/scala/uk/gov/hmrc/ct/ct600/v3/LoansToParticipatorsSpec.scala
+++ src/test/scala/uk/gov/hmrc/ct/ct600/v3/LoansToParticipatorsSpec.scala
@@ -16,11 +16,19 @@
 
 package uk.gov.hmrc.ct.ct600.v3
 
+import org.joda.time.LocalDate
 import org.scalatest.{Matchers, WordSpec}
+import uk.gov.hmrc.ct.computations.{CP2, AP2}
 import uk.gov.hmrc.ct.ct600.v3.stubs.StubbedCT600BoxRetriever
-import uk.gov.hmrc.ct.ct600a.v3.{Loan, LoansToParticipators}
+import uk.gov.hmrc.ct.ct600a.v3.{WriteOff, Loan, LoansToParticipators}
 
 class LoansToParticipatorsSpec extends WordSpec with Matchers {
+
+  val currentAPEndDate = new LocalDate(2015, 6, 1)
+
+  val boxRetriever = new StubbedCT600BoxRetriever {
+    override def retrieveCP2(): CP2 = CP2(currentAPEndDate)
+  }
 
   val validLoan = Loan(id = "1",
     name = "Smurfette",
@@ -29,7 +37,12 @@ class LoansToParticipatorsSpec extends WordSpec with Matchers {
     isRepaidAfter9Months = Some(false),
     hasWriteOffs = Some(false))
 
-  val boxRetriever = new StubbedCT600BoxRetriever {}
+  val validWriteOff = WriteOff(
+    id = "2",
+    date = currentAPEndDate.plusDays(2),
+    amount = 2000,
+    endDateOfAP = Some(currentAPEndDate.plusDays(1))
+  )
 
   "LoansToParticipators validate" should {
 
@@ -154,6 +167,66 @@ class LoansToParticipatorsSpec extends WordSpec with Matchers {
 
     "be happy if hasWriteOffs value is false" in {
       val l2pBox = LoansToParticipators(List(validLoan.copy(hasWriteOffs = Some(false))))
+      val errors = l2pBox.validate(boxRetriever)
+      errors.size shouldBe 0
+    }
+
+    "return an error if a write off has a date on the accounting period end date" in {
+      val l2pBox = LoansToParticipators(List(validLoan.copy(writeOffs = List(validWriteOff.copy(date = currentAPEndDate.minusDays(1))))))
+
+      val errors = l2pBox.validate(boxRetriever)
+      errors.size shouldBe 1
+      errors.head.boxId shouldBe Some("LoansToParticipators")
+      errors.head.errorMessageKey shouldBe "loan.1.writeOff.2.error.writeOff.date.range"
+    }
+
+    "return an error if a write off has a date after todays date" in {
+      val l2pBox = LoansToParticipators(List(validLoan.copy(writeOffs = List(validWriteOff.copy(date = LocalDate.now().plusDays(1))))))
+
+      val errors = l2pBox.validate(boxRetriever)
+      errors.size shouldBe 1
+      errors.head.boxId shouldBe Some("LoansToParticipators")
+      errors.head.errorMessageKey shouldBe "loan.1.writeOff.2.error.writeOff.date.range"
+    }
+
+    "not return an error if a write off has a date on todays date" in {
+      val l2pBox = LoansToParticipators(List(validLoan.copy(writeOffs = List(validWriteOff.copy(date = LocalDate.now())))))
+
+      val errors = l2pBox.validate(boxRetriever)
+      errors.size shouldBe 0
+    }
+
+    "return an error if a write off has an amount less then 1" in {
+      val l2pBox = LoansToParticipators(List(validLoan.copy(writeOffs = List(validWriteOff.copy(amount = 0)))))
+
+      val errors = l2pBox.validate(boxRetriever)
+      errors.size shouldBe 1
+      errors.head.boxId shouldBe Some("LoansToParticipators")
+      errors.head.errorMessageKey shouldBe "loan.1.writeOff.2.error.writeOff.amount.value"
+    }
+
+    "return an error if a write off has an amount greater then 99999999" in {
+      val l2pBox = LoansToParticipators(List(validLoan.copy(writeOffs = List(validWriteOff.copy(amount = 100000000)))))
+
+      val errors = l2pBox.validate(boxRetriever)
+      errors.size shouldBe 1
+      errors.head.boxId shouldBe Some("LoansToParticipators")
+      errors.head.errorMessageKey shouldBe "loan.1.writeOff.2.error.writeOff.amount.value"
+    }
+
+    "return no errors if a writeoff has a amount between 1 and 9999999 characters" in {
+      val l2pBox = LoansToParticipators(List(
+        validLoan.copy(writeOffs = List(validWriteOff.copy(amount = 1))),
+        validLoan.copy(writeOffs = List(validWriteOff.copy(amount = 99999999)))
+      ))
+
+      val errors = l2pBox.validate(boxRetriever)
+      errors.size shouldBe 0
+    }
+
+    "return no errors if a write off has a end date of AP after the current accounting period end date" in {
+      val l2pBox = LoansToParticipators(List(validLoan.copy(writeOffs = List(validWriteOff.copy(endDateOfAP = Some(LocalDate.now().plusDays(200)))))))
+
       val errors = l2pBox.validate(boxRetriever)
       errors.size shouldBe 0
     }
