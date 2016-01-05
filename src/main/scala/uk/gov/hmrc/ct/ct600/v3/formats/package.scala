5e56b271bf7e3f322ffd5677aaba12bfd7c42485
paul.campbell <paul.campbell@digital.hmrc.gov.uk>
2016-01-05 10:19:55 +0000

    CATO-3128 new boxes for form company info fields #4

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/formats/package.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/formats/package.scala
@@ -24,10 +24,6 @@ package object formats {
 
   implicit val b1Format: Format[B1] = new StringFormat[B1](B1.apply)
 
-  implicit val B30Format: Format[B30] = new DateFormat[B30](B30.apply)
-
-  implicit val B35Format: Format[B35] = new DateFormat[B35](B35.apply)
-
   implicit val B40Format: Format[B40] = new OptionalBooleanFormat[B40](B40.apply)
 
   implicit val B45Format: Format[B45Input] = new OptionalBooleanFormat[B45Input](B45Input.apply)
