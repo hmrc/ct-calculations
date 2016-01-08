f27803256c5d8a385c5d5182d056df92e4107bfd
James Williams <james@liquid-armour.co.uk>
2016-01-08 16:42:53 +0000

    CATO-3000 Added validation for E50 and E95

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E50.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E50.scala
@@ -16,6 +16,18 @@
 
 package uk.gov.hmrc.ct.ct600e.v3
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.ct600e.v3.retriever.CT600EBoxRetriever
 
 case class E50(value: Option[Int]) extends CtBoxIdentifier("Income Total turnover from exempt charitable trading activities") with CtOptionalInteger with Input
+
+object E50 extends ValidatableBox[CT600EBoxRetriever] {
+
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = {
+    import boxRetriever._
+    (retrieveE95(), retrieveE50()) match {
+      case (E95(Some(e95)), e50) if e95 > 0 && e50.orZero <= 0 => Set(CtValidation(boxId = Some("E50"), errorMessageKey = "error.E50.must.be.positive.when.E95.positive"))
+      case _ => Set.empty
+    }
+  }
+}
