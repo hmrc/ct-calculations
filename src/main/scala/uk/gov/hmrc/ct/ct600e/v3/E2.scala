01858e7e9cec9795ab82854ddf2caaaa22fd3ec6
James Williams <james@liquid-armour.co.uk>
2016-01-11 17:47:12 +0000

    CATO-3000 Mapping UTR into E2

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E2.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E2.scala
@@ -16,6 +16,11 @@
 
 package uk.gov.hmrc.ct.ct600e.v3
 
-import uk.gov.hmrc.ct.box.{Input, CtString, CtBoxIdentifier}
+import uk.gov.hmrc.ct.UTR
+import uk.gov.hmrc.ct.box.{Linked, Input, CtString, CtBoxIdentifier}
 
-case class E2(value: String) extends CtBoxIdentifier("Tax Reference - UTR") with CtString with Input
+case class E2(value: String) extends CtBoxIdentifier("Tax Reference - UTR") with CtString
+
+object E2 extends Linked[UTR, E2] {
+  override def apply(source: UTR): E2 = E2(source.value)
+}
