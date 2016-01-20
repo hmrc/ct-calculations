966b48433e6f40dc91cdf5f9fcbb918857e500d4
James Williams <james@liquid-armour.co.uk>
2016-01-20 18:28:34 +0000

    CATO-3000 Added declaration box retriever.
    Added box bvalues helper to companion object

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/retriever/CT600DeclarationBoxRetriever.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/retriever/CT600DeclarationBoxRetriever.scala
@@ -16,8 +16,10 @@
 
 package uk.gov.hmrc.ct.ct600.v3.retriever
 
-import uk.gov.hmrc.ct.box.retriever.BoxRetriever
+import uk.gov.hmrc.ct.box.retriever.{BoxValues, BoxRetriever}
 import uk.gov.hmrc.ct.ct600.v3.{B980, N092, B985, B975}
+
+object CT600DeclarationBoxRetriever extends BoxValues[CT600DeclarationBoxRetriever]
 
 trait CT600DeclarationBoxRetriever extends BoxRetriever {
 
