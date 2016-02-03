16508eb7cd2e5f83a1c51781458b3c261866b6ab
Allen Fensome <allen.fensome@gmail.com>
2016-02-03 16:28:27 +0000

    CATO-2873 Fix CP500

---
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP500.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ct.computations
+
+import uk.gov.hmrc.ct.box.{Calculated, CtBoxIdentifier, CtInteger}
+import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
+
+case class CP500(value: Int) extends CtBoxIdentifier(name = "Total Expenses") with CtInteger
+
+object CP500 extends Calculated[CP500, ComputationsBoxRetriever] {
+
+  override def calculate(boxRetriever: ComputationsBoxRetriever): CP500 = {
+    CP500(boxRetriever.retrieveCP39().value - boxRetriever.retrieveCP40().value)
+  }
+
+}
