517ab812ea75140deb04fef9cd0876ad470911ab
Allen Fensome <allen.fensome@gmail.com>
2016-02-03 15:09:11 +0000

    CATO-2873 Detailed profit and loss validation

--- src/main/scala/uk/gov/hmrc/ct/computations/CP48.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP48.scala
@@ -16,9 +16,14 @@
 
 package uk.gov.hmrc.ct.computations
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
-case class CP48(value: Option[Int]) extends CtBoxIdentifier(name = "Donations") with CtOptionalInteger with Input
+case class CP48(value: Option[Int]) extends CtBoxIdentifier(name = "Donations") with CtOptionalInteger with Input with ValidatableBox[ComputationsBoxRetriever] {
+
+  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = validateZeroOrPositiveInteger(this)
+
+}
 
 object CP48 {
 
