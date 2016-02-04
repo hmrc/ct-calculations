12ec161bf772e7f1eed80039f6e084987f3105d6
James Williams <james@liquid-armour.co.uk>
2016-02-04 12:27:37 +0000

    CATO-3000  More small changes.

--- src/main/scala/uk/gov/hmrc/ct/ct600/v2/B55.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v2/B55.scala
@@ -25,6 +25,7 @@ case class B55(value: BigDecimal) extends CtBoxIdentifier("Second Financial Year
 
 object B55 extends CorporationTaxCalculator with Calculated[B55, CT600BoxRetriever] {
 
+
   override def calculate(fieldValueRetriever: CT600BoxRetriever): B55 =
     rateOfTaxFy2(HmrcAccountingPeriod(fieldValueRetriever.retrieveCP1(), fieldValueRetriever.retrieveCP2()),
                  fieldValueRetriever.retrieveB37(),
