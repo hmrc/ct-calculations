a80920470fd7b419d863c176384b691e01122213
James Williams <james@liquid-armour.co.uk>
2016-02-08 17:04:44 +0000

    CATO-3000 Added boxes to hold years and tax rates for CT600E if no CT600 in scope.

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E3.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E3.scala
@@ -17,6 +17,6 @@
 package uk.gov.hmrc.ct.ct600e.v3
 
 import org.joda.time.LocalDate
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtDate, Input}
+import uk.gov.hmrc.ct.box.{CtBoxIdentifier, Input, StartDate}
 
-case class E3(value: LocalDate) extends CtBoxIdentifier("Accounting Period start date") with CtDate with Input
+case class E3(value: LocalDate) extends CtBoxIdentifier("Accounting Period start date") with StartDate with Input
--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E4.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E4.scala
@@ -17,6 +17,6 @@
 package uk.gov.hmrc.ct.ct600e.v3
 
 import org.joda.time.LocalDate
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtDate, Input}
+import uk.gov.hmrc.ct.box.{CtBoxIdentifier, EndDate, Input}
 
-case class E4(value: LocalDate) extends CtBoxIdentifier("Accounting Period end date") with CtDate with Input
+case class E4(value: LocalDate) extends CtBoxIdentifier("Accounting Period end date") with EndDate with Input
