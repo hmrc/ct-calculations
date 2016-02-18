eae51893843dcdd940a96bac9c4e90d0afebceb7
scott.cutts <scott.cutts@digital.hmrc.gov.uk>
2016-02-18 14:34:32 +0000

    CATO-3431: Fixed imports

--- src/main/scala/uk/gov/hmrc/ct/ct600/v2/B108.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v2/B108.scala
@@ -17,7 +17,7 @@
 package uk.gov.hmrc.ct.ct600.v2
 
 import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Linked}
-import uk.gov.hmrc.ct.computations.{CP247, CP91, CP291}
+import uk.gov.hmrc.ct.computations.CP247
 
 
 case class B108(value: Option[Int]) extends CtBoxIdentifier("Balancing charges (Machinery & Plant - main pool)") with CtOptionalInteger
