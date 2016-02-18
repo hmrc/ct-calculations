bf9fe1c43eaa282aec38b53b945e244b231b64a8
scott.cutts <scott.cutts@digital.hmrc.gov.uk>
2016-02-18 14:33:18 +0000

    CATO-3431: B108 now linked to correct box

--- src/main/scala/uk/gov/hmrc/ct/ct600/v2/B108.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v2/B108.scala
@@ -17,12 +17,12 @@
 package uk.gov.hmrc.ct.ct600.v2
 
 import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Linked}
-import uk.gov.hmrc.ct.computations.CP291
+import uk.gov.hmrc.ct.computations.{CP247, CP91, CP291}
 
 
 case class B108(value: Option[Int]) extends CtBoxIdentifier("Balancing charges (Machinery & Plant - main pool)") with CtOptionalInteger
 
-object B108 extends Linked[CP291, B108] {
+object B108 extends Linked[CP247, B108] {
 
-  override def apply(source: CP291): B108 = B108(source.value)
+  override def apply(source: CP247): B108 = B108(source.value)
 }
