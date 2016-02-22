4b69b1f1e0aafc23589afe87c26c36e5080e4938
Reece Bedding <reece.bedding@digital.voa.gov.uk>
2016-02-22 15:05:57 +0000

    CATO-3285: Refactored E85 validate override

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E85.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E85.scala
@@ -20,7 +20,7 @@ import uk.gov.hmrc.ct.box._
 import uk.gov.hmrc.ct.ct600e.v3.retriever.CT600EBoxRetriever
 
 case class E85(value: Option[Int]) extends CtBoxIdentifier("Income Other sources") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever]{
-  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = {
-    validatePositiveInteger(this)
-  }
+
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] =  validatePositiveInteger(this)
+
 }
