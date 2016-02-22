295ca2990feedb3aac87409f2bcde1c2a698153b
Tomasz Mikus <mikus.tomasz@gmail.com>
2016-02-22 15:48:44 +0000

    CATO-3288: Adding validation for 'E120'.

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E120.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E120.scala
@@ -16,6 +16,12 @@
 
 package uk.gov.hmrc.ct.ct600e.v3
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.ct600e.v3.retriever.CT600EBoxRetriever
 
-case class E120(value: Option[Int]) extends CtBoxIdentifier("Expenditure: Other expenditure not included above, or not used in calculating figures entered on the form CT600") with CtOptionalInteger with Input
+case class E120(value: Option[Int]) extends CtBoxIdentifier("Expenditure: Other expenditure not included above, or not used in calculating figures entered on the form CT600") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever] {
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = {
+    validatePositiveInteger(this)
+  }
+}
+
