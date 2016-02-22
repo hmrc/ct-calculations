0b01c9e9cd1bb613d562d955b9d632a0b58492fe
Tomasz Mikus <mikus.tomasz@gmail.com>
2016-02-22 13:07:09 +0000

    CATO-3287: Adding validation for 'E110' and 'E115'.

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E110.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E110.scala
@@ -16,6 +16,11 @@
 
 package uk.gov.hmrc.ct.ct600e.v3
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.ct600e.v3.retriever.CT600EBoxRetriever
 
-case class E110(value: Option[Int]) extends CtBoxIdentifier("Expenditure: All grants and donations made within the UK") with CtOptionalInteger with Input
+case class E110(value: Option[Int]) extends CtBoxIdentifier("Expenditure: All grants and donations made within the UK") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever] {
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = {
+    validatePositiveInteger(this)
+  }
+}
