8412b0637abe30fd480655f390360854d2190e43
scott.cutts <scott.cutts@digital.hmrc.gov.uk>
2016-02-22 11:58:43 +0000

    CATO-3283: Added zerOrPositive validation for Charity Gift boxes

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E75.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E75.scala
@@ -16,6 +16,9 @@
 
 package uk.gov.hmrc.ct.ct600e.v3
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.ct600e.v3.retriever.CT600EBoxRetriever
 
-case class E75(value: Option[Int]) extends CtBoxIdentifier("Income Gifts of shares or securities received") with CtOptionalInteger with Input
+case class E75(value: Option[Int]) extends CtBoxIdentifier("Income Gifts of shares or securities received") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever] {
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = validateZeroOrPositiveInteger(this)
+}
