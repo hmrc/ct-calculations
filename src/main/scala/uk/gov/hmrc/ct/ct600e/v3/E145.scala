fead430aee97890d3a525483a671e0624110c373
Reece Bedding <reece.bedding@digital.voa.gov.uk>
2016-02-23 12:32:06 +0000

    CATO-3322 Added new lines to help with readability

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E145.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E145.scala
@@ -20,5 +20,7 @@ import uk.gov.hmrc.ct.box._
 import uk.gov.hmrc.ct.ct600e.v3.retriever.CT600EBoxRetriever
 
 case class E145(value: Option[BigDecimal]) extends CtBoxIdentifier("Held at the end of the period (use accounts figures): UK investments (excluding controlled companies)") with CtOptionalBigDecimal with Input with ValidatableBox[CT600EBoxRetriever]{
+
   override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = validateZeroOrPositiveBigDecimal(this)
+
 }
