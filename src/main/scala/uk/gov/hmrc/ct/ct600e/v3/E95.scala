a41c1e28030bd0df723bfd7da936ac18e5226388
Tomasz Mikus <mikus.tomasz@gmail.com>
2016-02-23 11:40:07 +0000

    CATO-3252: Adding validation to 'E50' and 'E95'.

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E95.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E95.scala
@@ -16,6 +16,9 @@
 
 package uk.gov.hmrc.ct.ct600e.v3
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.ct600e.v3.retriever.CT600EBoxRetriever
 
-case class E95(value: Option[Int]) extends CtBoxIdentifier("Expenditure: Trading costs in relation to exempt charitable activities (in box E50)") with CtOptionalInteger with Input
+case class E95(value: Option[Int]) extends CtBoxIdentifier("Expenditure: Trading costs in relation to exempt charitable activities (in box E50)") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever] {
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = validateZeroOrPositiveInteger(this)
+}
