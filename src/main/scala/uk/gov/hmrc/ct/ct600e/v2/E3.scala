493e23119ac712ae7994ca8f4cc4d6f7079aa668
Allen Fensome <allen.fensome@gmail.com>
2016-03-03 13:56:58 +0000

    CATO-3344 Gift Aid V2 validation.

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v2/E3.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v2/E3.scala
@@ -16,6 +16,23 @@
 
 package uk.gov.hmrc.ct.ct600e.v2
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.ct600e.v2.retriever.CT600EBoxRetriever
 
-case class E3(value: Option[Int]) extends CtBoxIdentifier("Further repayment") with CtOptionalInteger with Input
+case class E3(value: Option[Int]) extends CtBoxIdentifier("Further repayment") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever] {
+
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = {
+    validateConditionalRequired(boxRetriever)
+  }
+
+  private def validateConditionalRequired(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = {
+    val e1 = boxRetriever.retrieveE1().value.getOrElse(0)
+    val e2 = boxRetriever.retrieveE2().value.getOrElse(0)
+
+    value match {
+      case None if e2 > e1 => Set(CtValidation(Some("E3"), s"error.E3.conditionalRequired"))
+      case _ => Set()
+    }
+  }
+
+}
---
+++ src/test/scala/uk/gov/hmrc/ct/ct600e/v2/E4Spec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ct.ct600e.v2
+
+import org.mockito.Mockito._
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.mock.MockitoSugar
+import uk.gov.hmrc.ct.box.CtValidation
+import uk.gov.hmrc.ct.ct600e.v2.retriever.CT600EBoxRetriever
+
+class E4Spec extends WordSpec with MockitoSugar with Matchers  {
+
+  "E4 validation" should {
+    "make E4 required when E2 < E1" in {
+      val boxRetriever = mock[CT600EBoxRetriever]
+      when(boxRetriever.retrieveE1()).thenReturn(E1(Some(2)))
+      when(boxRetriever.retrieveE2()).thenReturn(E2(Some(1)))
+      E4(None).validate(boxRetriever) shouldBe Set(CtValidation(Some("E4"), "error.E4.conditionalRequired", None))
+    }
+    "make E4 required when E2 empty but E1 is set" in {
+      val boxRetriever = mock[CT600EBoxRetriever]
+      when(boxRetriever.retrieveE1()).thenReturn(E1(Some(1)))
+      when(boxRetriever.retrieveE2()).thenReturn(E2(None))
+      E4(None).validate(boxRetriever) shouldBe Set(CtValidation(Some("E4"), "error.E4.conditionalRequired", None))
+    }
+
+    "make E4 not required when E2 > E1" in {
+      val boxRetriever = mock[CT600EBoxRetriever]
+      when(boxRetriever.retrieveE1()).thenReturn(E1(Some(1)))
+      when(boxRetriever.retrieveE2()).thenReturn(E2(Some(2)))
+      E4(None).validate(boxRetriever) shouldBe Set()
+    }
+    "make E4 not required when E2 set but E1 empty" in {
+      val boxRetriever = mock[CT600EBoxRetriever]
+      when(boxRetriever.retrieveE1()).thenReturn(E1(None))
+      when(boxRetriever.retrieveE2()).thenReturn(E2(Some(2)))
+      E4(None).validate(boxRetriever) shouldBe Set()
+    }
+    "make E4 not required when E2 and E1 are both empty" in {
+      val boxRetriever = mock[CT600EBoxRetriever]
+      when(boxRetriever.retrieveE1()).thenReturn(E1(None))
+      when(boxRetriever.retrieveE2()).thenReturn(E2(None))
+      E4(None).validate(boxRetriever) shouldBe Set()
+    }
+  }
+
+
+}
