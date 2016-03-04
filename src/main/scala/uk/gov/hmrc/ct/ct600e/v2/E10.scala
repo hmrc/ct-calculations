a769649aba4790d8a469342ef3dfd17c6e18e623
Tomasz Mikus <mikus.tomasz@gmail.com>
2016-03-04 11:01:52 +0000

    CATO-3447: Adding validation to a bunch of boxes.

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v2/E10.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v2/E10.scala
@@ -16,6 +16,11 @@
 
 package uk.gov.hmrc.ct.ct600e.v2
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.ct600e.v2.retriever.CT600EBoxRetriever
 
-case class E10(value: Option[Int]) extends CtBoxIdentifier("Income from other charities") with CtOptionalInteger with Input
+case class E10(value: Option[Int]) extends CtBoxIdentifier("Income from other charities") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever] {
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = {
+    validateZeroOrPositiveInteger(this)
+  }
+}
--- src/main/scala/uk/gov/hmrc/ct/ct600e/v2/E9.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v2/E9.scala
@@ -16,6 +16,11 @@
 
 package uk.gov.hmrc.ct.ct600e.v2
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.ct600e.v2.retriever.CT600EBoxRetriever
 
-case class E9(value: Option[Int]) extends CtBoxIdentifier("Gift Aid or Millennium Gift Aid") with CtOptionalInteger with Input
+case class E9(value: Option[Int]) extends CtBoxIdentifier("Gift Aid or Millennium Gift Aid") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever] {
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = {
+    validateZeroOrPositiveInteger(this)
+  }
+}
