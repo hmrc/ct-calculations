6d73f5b1946ee0ffa8afb6c52d022bd0e4d200bf
Allen Fensome <allen.fensome@gmail.com>
2016-03-04 11:12:02 +0000

    CATO-3344 Gift Aid V2 - zero not allowed.

--- src/test/scala/uk/gov/hmrc/ct/ct600e/v2/E3Spec.scala
+++ src/test/scala/uk/gov/hmrc/ct/ct600e/v2/E3Spec.scala
@@ -24,38 +24,46 @@ import uk.gov.hmrc.ct.ct600e.v2.retriever.CT600EBoxRetriever
 
 class E3Spec extends WordSpec with MockitoSugar with Matchers  {
 
+  val boxRetriever = mock[CT600EBoxRetriever]
+
   "E3 validation" should {
     "make E3 required when E2 > E1" in {
-      val boxRetriever = mock[CT600EBoxRetriever]
       when(boxRetriever.retrieveE1()).thenReturn(E1(Some(1)))
       when(boxRetriever.retrieveE2()).thenReturn(E2(Some(2)))
       E3(None).validate(boxRetriever) shouldBe Set(CtValidation(Some("E3"), "error.E3.conditionalRequired", None))
     }
     "make E3 required when E2 set but E1 is empty" in {
-      val boxRetriever = mock[CT600EBoxRetriever]
       when(boxRetriever.retrieveE1()).thenReturn(E1(None))
       when(boxRetriever.retrieveE2()).thenReturn(E2(Some(2)))
       E3(None).validate(boxRetriever) shouldBe Set(CtValidation(Some("E3"), "error.E3.conditionalRequired", None))
     }
 
     "make E3 not required when E2 < E1" in {
-      val boxRetriever = mock[CT600EBoxRetriever]
       when(boxRetriever.retrieveE1()).thenReturn(E1(Some(2)))
       when(boxRetriever.retrieveE2()).thenReturn(E2(Some(1)))
       E3(None).validate(boxRetriever) shouldBe Set()
     }
     "make E3 not required when E2 empty but E1 set" in {
-      val boxRetriever = mock[CT600EBoxRetriever]
       when(boxRetriever.retrieveE1()).thenReturn(E1(Some(1)))
       when(boxRetriever.retrieveE2()).thenReturn(E2(None))
       E3(None).validate(boxRetriever) shouldBe Set()
     }
 
     "make E3 not required when E2 and E1 are both empty" in {
-      val boxRetriever = mock[CT600EBoxRetriever]
       when(boxRetriever.retrieveE1()).thenReturn(E1(None))
       when(boxRetriever.retrieveE2()).thenReturn(E2(None))
       E3(None).validate(boxRetriever) shouldBe Set()
+    }
+
+    "E3 invalid if number less that 1" in {
+      when(boxRetriever.retrieveE1()).thenReturn(E1(None))
+      when(boxRetriever.retrieveE2()).thenReturn(E2(None))
+      E3(Some(0)).validate(boxRetriever) shouldBe Set(CtValidation(Some("E3"), "error.E3.outOfRange", None))
+    }
+    "E3 valid if number 1 or greater" in {
+      when(boxRetriever.retrieveE1()).thenReturn(E1(None))
+      when(boxRetriever.retrieveE2()).thenReturn(E2(None))
+      E3(Some(1)).validate(boxRetriever) shouldBe Set()
     }
   }
 
