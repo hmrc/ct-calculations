92bb0688e5167978c19ad17609b5804690c423c4
Tomasz Mikus <mikus.tomasz@gmail.com>
2016-03-07 14:12:55 +0000

    CATO-3335: Small refactoring.

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E180.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E180.scala
@@ -25,6 +25,13 @@ import uk.gov.hmrc.ct.ct600e.v3.retriever.CT600EBoxRetriever
  * SomeLoansAndInvestments = 2
  * NoLoansNorInvestments = 3
  */
+
 case class E180(value: Option[Int]) extends CtBoxIdentifier("Qualifying investments and loans") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever] {
   override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = validateAsMandatory(this)
+}
+
+object CharityLoansAndInvestments {
+  val AllLoansAndInvestments = 1
+  val SomeLoansAndInvestments = 2
+  val NoLoansNorInvestments = 3
 }
