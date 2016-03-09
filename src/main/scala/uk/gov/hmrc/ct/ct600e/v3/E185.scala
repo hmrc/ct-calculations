124f5e6b4c93867d5329042380fbee614d817010
Tomasz Mikus <mikus.tomasz@gmail.com>
2016-03-09 10:10:08 +0000

    CATO-3336: Changed validation type for E185.

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E185.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v3/E185.scala
@@ -19,9 +19,6 @@ package uk.gov.hmrc.ct.ct600e.v3
 import uk.gov.hmrc.ct.box._
 import uk.gov.hmrc.ct.ct600e.v3.retriever.CT600EBoxRetriever
 
-case class E185(value: Option[BigDecimal]) extends CtBoxIdentifier("Held at the end of the period (use accounts figures): Value of any non-qualifying investments and loans") with CtOptionalBigDecimal with Input with ValidatableBox[CT600EBoxRetriever] {
-  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = validateZeroOrPositiveBigDecimal(this)
+case class E185(value: Option[Int]) extends CtBoxIdentifier("Held at the end of the period (use accounts figures): Value of any non-qualifying investments and loans") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever] {
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = validatePositiveInteger(this)
 }
-
-
-
