cc6aad577a1c9ccfa5c16e70d7873b5c4805fc5c
James Williams <james@liquid-armour.co.uk>
2016-03-11 16:47:55 +0000

    CATO-3027 Fixed the type of the box retriever passed to validate

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/B45Input.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/B45Input.scala
@@ -17,12 +17,12 @@
 package uk.gov.hmrc.ct.ct600.v3
 
 import uk.gov.hmrc.ct.box._
-import uk.gov.hmrc.ct.ct600.v3.retriever.CT600BoxRetriever
+import uk.gov.hmrc.ct.ct600.v3.retriever.{AboutThisReturnBoxRetriever, CT600BoxRetriever}
 
 
 case class B45Input(value: Option[Boolean]) extends CtBoxIdentifier("Are you owed a repayment for an earlier period?")
-  with CtOptionalBoolean with Input with ValidatableBox[CT600BoxRetriever] {
+  with CtOptionalBoolean with Input with ValidatableBox[AboutThisReturnBoxRetriever] {
 
-  def validate(boxRetriever: CT600BoxRetriever): Set[CtValidation] = validateBooleanAsMandatory("B45Input", this)
+  def validate(boxRetriever: AboutThisReturnBoxRetriever): Set[CtValidation] = validateBooleanAsMandatory("B45Input", this)
 
 }
--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/B65.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/B65.scala
@@ -17,8 +17,8 @@
 package uk.gov.hmrc.ct.ct600.v3
 
 import uk.gov.hmrc.ct.box._
-import uk.gov.hmrc.ct.ct600.v3.retriever.CT600BoxRetriever
+import uk.gov.hmrc.ct.ct600.v3.retriever.{AboutThisReturnBoxRetriever, CT600BoxRetriever}
 
-case class B65(value: Option[Boolean]) extends CtBoxIdentifier("Notice of disclosable avoidance schemes") with CtOptionalBoolean with Input with ValidatableBox[CT600BoxRetriever] {
-  override def validate(boxRetriever: CT600BoxRetriever): Set[CtValidation] = validateAsMandatory(this)
+case class B65(value: Option[Boolean]) extends CtBoxIdentifier("Notice of disclosable avoidance schemes") with CtOptionalBoolean with Input with ValidatableBox[AboutThisReturnBoxRetriever] {
+  override def validate(boxRetriever: AboutThisReturnBoxRetriever): Set[CtValidation] = validateAsMandatory(this)
 }
