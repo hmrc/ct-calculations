56b1b1945acb6d7b9e9069c44c79a2ca054365be
Tomasz Mikus <mikus.tomasz@gmail.com>
2016-03-11 10:21:03 +0000

    CATO-3441: Adding some more fancy validation for E27.

--- src/main/scala/uk/gov/hmrc/ct/ct600e/v2/E27.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600e/v2/E27.scala
@@ -20,5 +20,12 @@ import uk.gov.hmrc.ct.box._
 import uk.gov.hmrc.ct.ct600e.v2.retriever.CT600EBoxRetriever
 
 case class E27(value: Option[Int]) extends CtBoxIdentifier("Value of any non-qualifying investments and loans") with CtOptionalInteger with Input with ValidatableBox[CT600EBoxRetriever] {
-  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = validatePositiveInteger(this)
+  override def validate(boxRetriever: CT600EBoxRetriever): Set[CtValidation] = {
+    value match {
+      case None => Set(CtValidation(Some(id), s"error.$id.required"))
+      case Some(x) if x == 0 => Set(CtValidation(Some(id), s"error.$id.required"))
+      case Some(x) if x < 0 => Set(CtValidation(Some(id), s"error.$id.mustBePositive"))
+      case _ => Set()
+    }
+  }
 }
