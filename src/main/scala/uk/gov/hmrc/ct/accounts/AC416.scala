0c3ea4b65334cfcc59d03cba4ec96673ebd70411
James Williams <james@liquid-armour.co.uk>
2016-04-05 16:31:00 +0100

    CATO-3006 These fields should allow negative values now

--- src/main/scala/uk/gov/hmrc/ct/accounts/AC416.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/AC416.scala
@@ -19,4 +19,4 @@ package uk.gov.hmrc.ct.accounts
 import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input, MustBeNoneOrZeroOrPositive}
 
 case class AC416(value: Option[Int]) extends CtBoxIdentifier(name = "Previous Staff costs")
-                                    with CtOptionalInteger with MustBeNoneOrZeroOrPositive with Input
+                                    with CtOptionalInteger with Input
