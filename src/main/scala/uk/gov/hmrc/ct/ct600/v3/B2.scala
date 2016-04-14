79bc6a1d4d628b2239be45b11a98bf22a6e4c3b8
James Williams <james@liquid-armour.co.uk>
2016-04-14 15:47:06 +0100

    CATO-3633 Made B2 optional.

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/B2.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/B2.scala
@@ -17,10 +17,10 @@
 package uk.gov.hmrc.ct.ct600.v3
 
 import uk.gov.hmrc.ct.accounts.AC1
-import uk.gov.hmrc.ct.box.{Linked, CtBoxIdentifier, CtString}
+import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalString, Linked}
 
 
-case class B2(value: Option[String]) extends CtBoxIdentifier(name = "Company Registration Number (CRN)") with CtString
+case class B2(value: Option[String]) extends CtBoxIdentifier(name = "Company Registration Number (CRN)") with CtOptionalString
 
 object B2 extends Linked[AC1, B2] {
 
