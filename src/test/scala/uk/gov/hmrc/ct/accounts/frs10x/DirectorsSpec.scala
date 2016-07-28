7064169deedac9a914cbd624d517fba295657b34
Tomasz Mikus <mikus.tomasz@gmail.com>
2016-07-28 09:13:27 +0100

    CATO-3872: Fixing the :eggy:

--- src/test/scala/uk/gov/hmrc/ct/accounts/frs10x/DirectorsSpec.scala
+++ src/test/scala/uk/gov/hmrc/ct/accounts/frs10x/DirectorsSpec.scala
@@ -135,6 +135,13 @@ class DirectorsSpec extends WordSpec with MockitoSugar with Matchers with Before
       directors.validate(mockBoxRetriever) shouldBe expectedError
     }
 
+    "no error if at least one director appointed if are-there-appointments question is yes" in {
+      when(mockBoxRetriever.retrieveACQ8003()).thenReturn(ACQ8003(Some(true)))
+      val directors = Directors(List(Director("444", "Jack", ac8005 = Some(true)), Director("555", "Jill")))
+
+      directors.validate(mockBoxRetriever) shouldBe empty
+    }
+
     "do not validate at least one director appointed if are-there-appointments question is no" in {
 
       val directors = Directors(List(Director("444", "Jack"), Director("555", "Jill")))
@@ -150,9 +157,9 @@ class DirectorsSpec extends WordSpec with MockitoSugar with Matchers with Before
       directors.validate(mockBoxRetriever) shouldBe expectedError
     }
 
-    "no error if at least one director appointed if are-there-appointments question is yes" in {
-      when(mockBoxRetriever.retrieveACQ8003()).thenReturn(ACQ8003(Some(true)))
-      val directors = Directors(List(Director("444", "Jack", ac8005 = Some(true)), Director("555", "Jill")))
+    "no error if at least one director appointed if are-there-resignations question is yes" in {
+      when(mockBoxRetriever.retrieveACQ8009()).thenReturn(ACQ8009(Some(true)))
+      val directors = Directors(List(Director("444", "Jack", ac8011 = Some(true)), Director("555", "Jill")))
 
       directors.validate(mockBoxRetriever) shouldBe empty
     }
