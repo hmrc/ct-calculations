3e9d82b8414ce86c4bc0780870bdcceda3ab78e3
James Williams <james@liquid-armour.co.uk>
2016-10-28 14:33:43 +0100

    CATO-4168 Added box AC7210 to calculations.

---
+++ src/test/scala/uk/gov/hmrc/ct/accounts/frs102/boxes/AC7210Spec.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ct.accounts.frs102.boxes
+
+import org.mockito.Mockito.when
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{Matchers, WordSpec}
+import uk.gov.hmrc.ct.accounts.frs102.{AccountsFreeTextSizeRangeValidationFixture, AccountsFreeTextValidationFixture, MockFrs102AccountsRetriever}
+import uk.gov.hmrc.ct.box.CtValidation
+import uk.gov.hmrc.ct.box.ValidatableBox._
+
+class AC7210Spec extends WordSpec with MockitoSugar with Matchers with MockFrs102AccountsRetriever with AccountsFreeTextSizeRangeValidationFixture {
+
+  override def setUpMocks(): Unit = {
+    when(boxRetriever.ac32()).thenReturn(AC32(Some(4)))
+  }
+
+  testAccountsCharacterSizeRangeValidation("AC7210", 0, StandardCohoTextFieldLimit, AC7210)
+  testAccountsCoHoTextFieldValidation("AC7210", AC7210)
+
+  "AC7210" should {
+
+    "fail validation when populated and AC32 is empty" in {
+      when(boxRetriever.ac32()).thenReturn(AC32(None))
+      AC7210(Some("testing")).validate(boxRetriever) shouldBe Set(CtValidation(Some("AC7210"), "error.AC7210.cannot.exist"))
+    }
+  }
+}
