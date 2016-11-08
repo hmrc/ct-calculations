3039e8e05620874cc706a4f28083ad3a52db5d9f
Mario Paniccia <mario.paniccia@digital.hmrc.gov.uk>
2016-11-08 16:22:56 +0000

    CATO-4220: added box AC7995. Refactorings

---
+++ src/main/scala/uk/gov/hmrc/ct/accounts/frs105/boxes/AC7995.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ct.accounts.frs105.boxes
+
+import uk.gov.hmrc.ct.accounts.frs105.retriever.Frs105AccountsBoxRetriever
+import uk.gov.hmrc.ct.box.ValidatableBox._
+import uk.gov.hmrc.ct.box._
+
+case class AC7995(value: Option[String]) extends CtBoxIdentifier(name = "Commitments by way of guarantee note") with CtOptionalString
+with Input
+with SelfValidatableBox[Frs105AccountsBoxRetriever, Option[String]]
+with Validators {
+
+  override def validate(boxRetriever: Frs105AccountsBoxRetriever) = {
+    import boxRetriever._
+    collectErrors (
+      cannotExistIf(value.nonEmpty && ac7991().isFalse),
+
+      failIf (boxRetriever.ac7991().isTrue) (
+        collectErrors (
+          validateStringAsMandatory(),
+          validateOptionalStringByLength(1, StandardCohoTextFieldLimit),
+          validateCoHoOptionalString()
+        )
+      )
+    )
+  }
+}
