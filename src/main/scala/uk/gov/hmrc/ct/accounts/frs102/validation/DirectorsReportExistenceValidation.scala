a056210010f70406af31490edf987ecd25b8c6b7
scott.cutts <scott.cutts@digital.hmrc.gov.uk>
2016-11-18 11:03:04 +0000

    CATO-4441: Tidied up DirectorsReportExistence validator

--- src/main/scala/uk/gov/hmrc/ct/accounts/frs102/validation/DirectorsReportExistenceValidation.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/frs102/validation/DirectorsReportExistenceValidation.scala
@@ -17,34 +17,33 @@
 package uk.gov.hmrc.ct.accounts.frs102.validation
 
 import uk.gov.hmrc.ct.accounts.frs102.retriever.Frs10xDirectorsBoxRetriever
-import uk.gov.hmrc.ct.box.CtValidation
+import uk.gov.hmrc.ct.box.{CtValidation, Validators}
 import uk.gov.hmrc.ct.box.retriever.BoxRetriever._
 import uk.gov.hmrc.ct.box.retriever.FilingAttributesBoxValueRetriever
 
-trait DirectorsReportExistenceValidation {
+trait DirectorsReportExistenceValidation extends Validators {
 
-  def validateDirectorsReportCannotExist(boxId: String, value: Option[Boolean], boxRetriever: Frs10xDirectorsBoxRetriever with FilingAttributesBoxValueRetriever)(): Set[CtValidation] = {
-    import boxRetriever._
-
-    if (value.contains(false)) {
-      val noteNonEmpty =
-        directors().directors.nonEmpty ||
-          anyHaveValue(
-            acQ8003(),
-            ac8033(),
-            acQ8009(),
-            ac8051(),
-            ac8052(),
-            ac8053(),
-            ac8054(),
-            ac8899()
-          )
-
-      if (noteNonEmpty)
-        Set(CtValidation(None, s"error.$boxId.directorsReport.cannot.exist"))
-      else
-        Set.empty
-    } else Set.empty
+  def validateDirectorsReportCannotExist(boxId: String,
+                                         includeDirectorsReportValue: Option[Boolean],
+                                         boxRetriever: Frs10xDirectorsBoxRetriever with FilingAttributesBoxValueRetriever)(): Set[CtValidation] = {
+    failIf(includeDirectorsReportValue.contains(false) && directorsReportPopulated(boxRetriever)) {
+      Set(CtValidation(None, s"error.$boxId.directorsReport.cannot.exist"))
+    }
   }
 
+  def directorsReportPopulated(boxRetriever: Frs10xDirectorsBoxRetriever with FilingAttributesBoxValueRetriever): Boolean = {
+    import boxRetriever._
+
+    directors().directors.nonEmpty ||
+      anyHaveValue(
+        acQ8003(),
+        ac8033(),
+        acQ8009(),
+        ac8051(),
+        ac8052(),
+        ac8053(),
+        ac8054(),
+        ac8899()
+      )
+  }
 }
