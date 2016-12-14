36ab6803df103e0578ce56ff9f539538276f6c40
James Williams <james@liquid-armour.co.uk>
2016-12-14 17:00:40 +0000

    CATO-4546 Moving the formatters for the approval boxes.

--- src/main/scala/uk/gov/hmrc/ct/accounts/frs10x/formats/package.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/frs10x/formats/package.scala
@@ -23,8 +23,6 @@ import uk.gov.hmrc.ct.box.formats._
 
 package object formats {
 
-  private def withDefault[A](key:String, default:A)(implicit writes:Writes[A]) = __.json.update((__ \ key).json.copyFrom((__ \ key).json.pick orElse Reads.pure(Json.toJson(default))))
-
   implicit val ac8081Format = new OptionalBooleanFormat(AC8081.apply)
   implicit val ac8082Format = new OptionalBooleanFormat(AC8082.apply)
   implicit val ac8083Format = new OptionalBooleanFormat(AC8083.apply)
@@ -32,30 +30,4 @@ package object formats {
 
   implicit val acq8161Format = new OptionalBooleanFormat[ACQ8161](ACQ8161.apply)
 
-  implicit val ac8092Format = new OptionalStringFormat[AC8092](AC8092.apply)
-  implicit val ac8091Format = new OptionalBooleanFormat[AC8091](AC8091.apply)
-  implicit val ac198AFormat = new OptionalDateFormat[AC198A](AC198A.apply)
-  implicit val ac199AFormat = new StringFormat[AC199A](AC199A.apply)
-
-  implicit val coHoAccountsApprovalFormatWithDefaults = new Format[CompaniesHouseAccountsApproval] {
-    val baseFormat = Json.format[CompaniesHouseAccountsApproval]
-
-    override def reads(json: JsValue): JsResult[CompaniesHouseAccountsApproval] = baseFormat
-      .compose(withDefault("ac8091", AC8091(None)))
-      .compose(withDefault("ac198A", AC198A(None)))
-      .reads(json)
-
-    override def writes(o: CompaniesHouseAccountsApproval): JsValue = baseFormat.writes(o)
-  }
-
-  implicit val hmrcAccountsApprovalFormatWithDefaults = new Format[HmrcAccountsApproval] {
-    val baseFormat = Json.format[HmrcAccountsApproval]
-
-    override def reads(json: JsValue): JsResult[HmrcAccountsApproval] = baseFormat
-      .compose(withDefault("ac8091", AC8091(None)))
-      .compose(withDefault("ac198A", AC198A(None)))
-      .reads(json)
-
-    override def writes(o: HmrcAccountsApproval): JsValue = baseFormat.writes(o)
-  }
 }
