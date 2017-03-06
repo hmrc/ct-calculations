35e2b0a110aaef5f0dfbae7a6ac17838e95298c7
Tomasz Mikus <mikus.tomasz@gmail.com>
2017-03-06 12:25:11 +0000

    CATO-4485: Adding validation for AC71.

--- src/main/scala/uk/gov/hmrc/ct/accounts/frs102/boxes/AC71.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/frs102/boxes/AC71.scala
@@ -18,16 +18,28 @@ package uk.gov.hmrc.ct.accounts.frs102.boxes
 
 import uk.gov.hmrc.ct.accounts.frs102.retriever.Frs102AccountsBoxRetriever
 import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.box.retriever.FilingAttributesBoxValueRetriever
 
 case class AC71(value: Option[Int]) extends CtBoxIdentifier(name = "Called up share capital (previous PoA)")
   with CtOptionalInteger
   with Input
-  with ValidatableBox[Frs102AccountsBoxRetriever]
+  with ValidatableBox[Frs102AccountsBoxRetriever with FilingAttributesBoxValueRetriever]
   with Validators {
 
-  override def validate(boxRetriever: Frs102AccountsBoxRetriever): Set[CtValidation] = {
+  override def validate(boxRetriever: Frs102AccountsBoxRetriever with FilingAttributesBoxValueRetriever): Set[CtValidation] = {
+    val limitedByGuarantee = boxRetriever.companyType().isLimitedByGuarantee
+    val hasPY = boxRetriever.ac205().hasValue
     collectErrors(
-      validateMoney(value, min = 1)
+      failIf(!hasPY || limitedByGuarantee)(
+        cannotExistIf(value.nonEmpty)
+      ),
+      failIf(hasPY && !limitedByGuarantee)(
+        collectErrors(
+          validateAsMandatory(this),
+          validateMoney(value, min = 1)
+        )
+      )
     )
   }
+
 }
