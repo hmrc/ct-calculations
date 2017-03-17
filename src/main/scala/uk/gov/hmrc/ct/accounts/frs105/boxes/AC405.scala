8e6c8c9489af96fe5806aa7c2ad12a486e4b2cdf
paul.campbell <paul.campbell@digital.hmrc.gov.uk>
2017-03-17 05:31:14 -0700

    CATO-4580 dont validate P&L when dormant

--- src/main/scala/uk/gov/hmrc/ct/accounts/frs105/boxes/AC405.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/frs105/boxes/AC405.scala
@@ -17,17 +17,17 @@
 package uk.gov.hmrc.ct.accounts.frs105.boxes
 
 import uk.gov.hmrc.ct.accounts.frs105.retriever.Frs105AccountsBoxRetriever
-import uk.gov.hmrc.ct.accounts.frs10x.retriever.Frs10xFilingQuestionsBoxRetriever
+import uk.gov.hmrc.ct.accounts.frs10x.retriever.{Frs10xDormancyBoxRetriever, Frs10xFilingQuestionsBoxRetriever}
 import uk.gov.hmrc.ct.box._
 import uk.gov.hmrc.ct.box.retriever.FilingAttributesBoxValueRetriever
 
 case class AC405(value: Option[Int]) extends CtBoxIdentifier(name = "Other income (current PoA)")
   with CtOptionalInteger
   with Input
-  with ValidatableBox[Frs105AccountsBoxRetriever with FilingAttributesBoxValueRetriever with Frs10xFilingQuestionsBoxRetriever] {
+  with ValidatableBox[Frs105AccountsBoxRetriever with FilingAttributesBoxValueRetriever with Frs10xFilingQuestionsBoxRetriever with Frs10xDormancyBoxRetriever] {
 
-  override def validate(boxRetriever: Frs105AccountsBoxRetriever with FilingAttributesBoxValueRetriever with Frs10xFilingQuestionsBoxRetriever): Set[CtValidation] = {
-    failIf(boxRetriever.hmrcFiling().value || boxRetriever.acq8161().orFalse) (
+  override def validate(boxRetriever: Frs105AccountsBoxRetriever with FilingAttributesBoxValueRetriever with Frs10xFilingQuestionsBoxRetriever with Frs10xDormancyBoxRetriever): Set[CtValidation] = {
+    failIf(!boxRetriever.acq8999().orFalse && (boxRetriever.hmrcFiling().value || boxRetriever.acq8161().orFalse)) (
       collectErrors(
         validateMoney(value),
         validateAtLeastOneCurrentYearFieldPopulated(boxRetriever)
@@ -35,7 +35,7 @@ case class AC405(value: Option[Int]) extends CtBoxIdentifier(name = "Other incom
     )
   }
 
-  def validateAtLeastOneCurrentYearFieldPopulated(boxRetriever: Frs105AccountsBoxRetriever with FilingAttributesBoxValueRetriever with Frs10xFilingQuestionsBoxRetriever)(): Set[CtValidation] = {
+  def validateAtLeastOneCurrentYearFieldPopulated(boxRetriever: Frs105AccountsBoxRetriever with FilingAttributesBoxValueRetriever with Frs10xFilingQuestionsBoxRetriever with Frs10xDormancyBoxRetriever)(): Set[CtValidation] = {
 
     val anyCurrentYearFieldHasAValue = (
         boxRetriever.ac12().value orElse
