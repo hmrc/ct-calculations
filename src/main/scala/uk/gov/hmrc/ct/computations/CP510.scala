ff0626374d4772d2d12198d4f817ec243d72e1a2
greg <greg.gajda@digital.hmrc.gov.uk>
2017-08-09 14:59:55 +0100

    CATO-3840 Validation of CP510 for de-block income from property

--- src/main/scala/uk/gov/hmrc/ct/computations/CP510.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP510.scala
@@ -16,6 +16,26 @@
 
 package uk.gov.hmrc.ct.computations
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
-case class CP510(value: Option[Int]) extends CtBoxIdentifier(name = "Unallowable expenses") with CtOptionalInteger with Input
+case class CP510(value: Option[Int]) extends CtBoxIdentifier(name = "Unallowable expenses") with CtOptionalInteger
+  with Input with ValidatableBox[ComputationsBoxRetriever] {
+  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = {
+    collectErrors(
+      validateZeroOrPositiveInteger(this),
+      unAllowableError(boxRetriever)
+    )
+  }
+
+  private def unAllowableError(boxRetriever: ComputationsBoxRetriever) = {
+    failIf(hasValue && value.get > boxRetriever.cp508().value) {
+      Set(CtValidation(None, "block.incomeFromProperty.unAllowable.error"))
+    }
+  }
+
+}
+
+object CP510 {
+  def apply(value: Int): CP510 = CP510(Some(value))
+}
