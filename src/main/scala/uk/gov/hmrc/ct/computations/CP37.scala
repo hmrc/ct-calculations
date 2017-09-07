ce73030c9ffc639325c12da67fae933c8d2b0712
greg <greg.gajda@digital.hmrc.gov.uk>
2017-09-07 16:01:49 +0100

    CATO-3841 Fields validation for de-block expenses

--- src/main/scala/uk/gov/hmrc/ct/computations/CP37.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP37.scala
@@ -16,9 +16,15 @@
 
 package uk.gov.hmrc.ct.computations
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Input}
+import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
 case class CP37(value: Option[Int]) extends CtBoxIdentifier(name = "Sundry expenses (use only for small miscellaneous expenses)") with CtOptionalInteger with Input
+  with ValidatableBox[ComputationsBoxRetriever] {
+  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = {
+    validateZeroOrPositiveInteger(this)
+  }
+}
 
 object CP37 {
 
