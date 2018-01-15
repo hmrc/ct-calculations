e64e3a4f27783fcec51729bc801601182d97724e
Tenoch De La Cruz <tenoch.delacruz@digital.hmrc.gov.uk>
2018-01-15 14:18:05 +0000

    Updated copyright header

--- src/main/scala/uk/gov/hmrc/ct/computations/CP283b.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP283b.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,4 +39,3 @@ object CP283b {
 
   def apply(int: Int): CP283b = CP283b(Some(int))
 }
-
