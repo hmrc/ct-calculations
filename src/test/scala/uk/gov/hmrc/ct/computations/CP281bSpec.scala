fd5a7896995be9a7a0132aa465d2134bea7ac119
Rut Patel <rut.patel@digital.hmrc.gov.uk>
2018-03-26 12:04:26 +0100

    CATOC-132
    Restored the unit test for CP997 and CP281b

--- src/test/scala/uk/gov/hmrc/ct/computations/CP281bSpec.scala
+++ src/test/scala/uk/gov/hmrc/ct/computations/CP281bSpec.scala
@@ -16,7 +16,54 @@
 
 package uk.gov.hmrc.ct.computations
 
+import org.joda.time.LocalDate
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{Matchers, WordSpec}
+import uk.gov.hmrc.ct.BoxValidationFixture
+import uk.gov.hmrc.ct.box.CtValidation
+import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
+import losses.lossReform2017
+import org.mockito.Mockito.when
 
-class CP281bSpec extends WordSpec with Matchers with MockitoSugar
+class CP281bSpec extends WordSpec with Matchers with MockitoSugar with BoxValidationFixture[ComputationsBoxRetriever] {
+
+  override val boxRetriever = makeBoxRetriever()
+
+  testMandatoryWhen("CP281b", CP281b.apply) {
+    makeBoxRetriever()
+  }
+
+  testBoxIsZeroOrPositive("CP281b", CP281b.apply)
+
+  testCannotExistWhen("CP281b", CP281b.apply, testDetails = "CPQ17 false") {
+    makeBoxRetriever(cpq17Value = false)
+  }
+
+  testCannotExistWhen("CP281b", CP281b.apply, testDetails = "before loss reform") {
+    makeBoxRetriever(cp2Value = lossReform2017)
+  }
+
+  "CP281a" should {
+    "fail if the sum of CP283b, CP288b and CP997 is less than CP281a" in {
+      CP281b(4).validate(makeBoxRetriever()).contains(CtValidation(None, "error.CP281b.breakdown.sum.incorrect")) shouldBe true
+    }
+    "fail if the sum of CP283b, CP288b and CP997 is greater than CP281a" in {
+      CP281b(2).validate(makeBoxRetriever()).contains(CtValidation(None, "error.CP281b.breakdown.sum.incorrect")) shouldBe true
+    }
+    "pass if the sum of CP283b, CP288b and CP997 is equal to CP281a" in {
+      CP281b(3).validate(makeBoxRetriever()).contains(CtValidation(None, "error.CP281b.breakdown.sum.incorrect")) shouldBe false
+    }
+  }
+
+  private def makeBoxRetriever(cp2Value: LocalDate = lossReform2017.plusDays(1), cpq17Value: Boolean = true,
+                               cp283bValue: Option[Int] = Some(1), cp288bValue: Option[Int] = Some(1), cp997Value: Option[Int] = Some(1)) = {
+    val retriever = mock[ComputationsBoxRetriever]
+    when(retriever.cp2()).thenReturn(CP2(cp2Value))
+    when(retriever.cpQ17()).thenReturn(CPQ17(Some(cpq17Value)))
+    when(retriever.cp283b()).thenReturn(CP283b(cp283bValue))
+    when(retriever.cp288b()).thenReturn(CP288b(cp288bValue))
+    when(retriever.cp997()).thenReturn(CP997(cp997Value))
+    retriever
+  }
+
+}
