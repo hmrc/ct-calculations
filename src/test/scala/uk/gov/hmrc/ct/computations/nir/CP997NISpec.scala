17f3a87a81c8edad2026f534b77d62a3fb47f7bc
Rut Patel <rut.patel@digital.hmrc.gov.uk>
2018-03-27 11:43:17 +0100

    CATOC-42v1
    Fixed the test description

--- src/test/scala/uk/gov/hmrc/ct/computations/nir/CP997NISpec.scala
+++ src/test/scala/uk/gov/hmrc/ct/computations/nir/CP997NISpec.scala
@@ -42,19 +42,19 @@ class CP997NISpec extends WordSpec with Matchers with MockitoSugar with Northern
 
   "CP997NI" should {
 
-    "be cp997d + cp997e if CPQ117 = true, b5 = true and value for CP997d and e are there" in {
+    "be cp997d + cp997e if CPQ117 = true, b7 = true and value for CP997d and e are there" in {
       CP997NI.calculate(makeBoxRetriever()) shouldBe CP997NI(Some(400))
     }
-    "be cp997d + cp997e if CPQ117 = false, b5 = true and value for CP997d and e are there" in {
+    "be cp997d + cp997e if CPQ117 = false, b7 = true and value for CP997d and e are there" in {
       CP997NI.calculate(makeBoxRetriever(Some(false), 1000, Some(200), Some(200), Some(true))) shouldBe CP997NI(Some(400))
     }
-    "be cp997d + cp997e CPQ117 = true, b5 = false and value for CP997d and e are there." in {
+    "be cp997d + cp997e CPQ117 = true, b7 = false and value for CP997d and e are there." in {
       CP997NI.calculate(makeBoxRetriever(Some(true), 1000, Some(200), Some(200), Some(false))) shouldBe CP997NI(Some(400))
     }
-    "be  cp997d if CPQ117 = false, b5 = false and value for CP997d and e isn't there." in {
+    "be  cp997d if CPQ117 = false, b7 = false and value for CP997d and e isn't there." in {
       CP997NI.calculate(makeBoxRetriever(Some(true), 1000, Some(200), Some(0), Some(false))) shouldBe CP997NI(Some(200))
     }
-    "be Some(0) if CPQ117 = false, b5 = false and value of CP997d and CP997e aren't there" in {
+    "be Some(0) if CPQ117 = false, b7 = false and value of CP997d and CP997e aren't there" in {
       CP997NI.calculate(makeBoxRetriever(Some(true), 1000, None, None, Some(false))) shouldBe CP997NI(Some(0))
     }
     "be None if CATO01 = 0"in {
