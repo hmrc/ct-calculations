1f0ba63b694bc6a27b1b11fec761e4ad0336f54a
Rut Patel <rut.patel@digital.hmrc.gov.uk>
2018-03-29 12:02:37 +0100

    CATOC-42v1
    Fixed the calculation for B430 when NIR is active.

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/B430.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/B430.scala
@@ -19,12 +19,24 @@ package uk.gov.hmrc.ct.ct600.v3
 import uk.gov.hmrc.ct.box.{Calculated, CtBigDecimal, CtBoxIdentifier}
 import uk.gov.hmrc.ct.ct600.v3.calculations.CorporationTaxCalculator
 import uk.gov.hmrc.ct.ct600.v3.retriever.CT600BoxRetriever
+import uk.gov.hmrc.ct.computations.losses._
 
 // was b63
 case class B430(value: BigDecimal) extends CtBoxIdentifier("Corporation Tax") with CtBigDecimal
 
 object B430 extends CorporationTaxCalculator with Calculated[B430, CT600BoxRetriever] {
 
-  override def calculate(fieldValueRetriever: CT600BoxRetriever): B430 =
-    calculateCorporationTax(fieldValueRetriever.b345(), fieldValueRetriever.b395())
+  override def calculate(fieldValueRetriever: CT600BoxRetriever): B430 = {
+
+    if(northernIrelandJourneyActive(fieldValueRetriever)){
+
+      calculateCorportationTaxForNIJourney(fieldValueRetriever.b345(), fieldValueRetriever.b360(),fieldValueRetriever.b395(), fieldValueRetriever.b410())
+
+    }else{
+
+      calculateCorporationTax(fieldValueRetriever.b345(), fieldValueRetriever.b395())
+    }
+
+  }
+
 }
