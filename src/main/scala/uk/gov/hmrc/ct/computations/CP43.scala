b61c26047f1a5c1457664ee2a8d5257a00b657f1
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2019-11-07 16:02:52 +0000

    [LG][DLS-1272] Reduced valid number in CP43 box

--- src/main/scala/uk/gov/hmrc/ct/computations/CP43.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP43.scala
@@ -21,12 +21,11 @@ import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
 case class CP43(value: Option[Int]) extends CtBoxIdentifier(name = "Interest Received") with CtOptionalInteger with Input with ValidatableBox[ComputationsBoxRetriever] {
 
-  override def validate(boxRetriever: ComputationsBoxRetriever) = validateZeroOrPositiveInteger(this)
-
+  override def validate(boxRetriever: ComputationsBoxRetriever) = {
+      validateIntegerRange(boxId, this, 0, 99999999)
+  }
 }
 
 object CP43 {
-
   def apply(int: Int): CP43 = CP43(Some(int))
-
 }
