af09afdfa2eb8deda2f02b5ff738db09001184f1
Alok <alok.acharya@digital.hmrc.gov.uk>
2019-12-18 10:45:55 +0000

    DLS-1277: changed name for package

--- src/main/scala/uk/gov/hmrc/ct/computations/sba/package.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/sba/package.scala
@@ -1,9 +1,9 @@
 package uk.gov.hmrc.ct.computations
 
 import org.joda.time.LocalDate
-import uk.gov.hmrc.ct.box.{EndDate, StartDate}
+import uk.gov.hmrc.ct.box.StartDate
 
-package object capitalAllowanceAndSBA {
+package object sba {
 
   val sbaApplies2019 = LocalDate.parse("2018-10-29")
 
