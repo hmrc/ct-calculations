72fd360bc21f23f1622e1b0177d8806e2b5108ba
Alok <alok.acharya@digital.hmrc.gov.uk>
2020-02-10 11:12:32 +0000

    DLS-1279: Fixed having two boxes doing the same thing

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/B711.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/B711.scala
@@ -17,13 +17,13 @@
 package uk.gov.hmrc.ct.ct600.v3
 
 import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Linked}
-import uk.gov.hmrc.ct.computations.CP296
+import uk.gov.hmrc.ct.computations.{CP296, CP297}
 
 
 case class B711(value: Option[Int]) extends CtBoxIdentifier with CtOptionalInteger
 
 
-object B711 extends Linked[CP296, B711] {
+object B711 extends Linked[CP297, B711] {
 
-  override def apply(source: CP296): B711 = B711(source.value)
+  override def apply(source: CP297): B711 = B711(source.value)
 }
