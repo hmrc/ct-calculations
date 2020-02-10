16631f60c8c5df23ebe38489889ed83a338cf909
Alok Acharya <31273756+AlokHMRC@users.noreply.github.com>
2020-02-10 11:17:42 +0000

    Dls 1279 (#477)
    * DLS-1279: Fixed having two boxes doing the same thing
    
    * DLS-1279: Fixed having two boxes doing the same thing

--- src/main/scala/uk/gov/hmrc/ct/computations/CP298.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP298.scala
@@ -20,11 +20,16 @@ import uk.gov.hmrc.ct.box.{Calculated, CtBoxIdentifier, CtOptionalInteger}
 import uk.gov.hmrc.ct.computations.calculations.SBACalculator
 import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
-case class CP298(value: Option[Int]) extends CtBoxIdentifier with CtOptionalInteger
+case class CP298(value: Option[Int]) extends CtBoxIdentifier("Total Relief Claimable") with CtOptionalInteger
 
 object CP298 extends Calculated[CP298, ComputationsBoxRetriever] with SBACalculator {
 
-  def getCostOfAllBuildings(computationsBoxRetriever: ComputationsBoxRetriever) = computationsBoxRetriever.sba01().buildings.map(_.cost)
+  def getTotalRelief(computationsBoxRetriever: ComputationsBoxRetriever): List[Option[Int]] = {
+    computationsBoxRetriever.sba01().buildings.map(building =>
 
-  override def calculate(boxRetriever: ComputationsBoxRetriever): CP298 = CP298(sumAmount(getCostOfAllBuildings(boxRetriever)))
+      getAmountClaimableForSBA(computationsBoxRetriever.cp1().value, computationsBoxRetriever.cp2().value, building.earliestWrittenContract, building.cost)
+    )
+  }
+
+  override def calculate(boxRetriever: ComputationsBoxRetriever): CP298 = CP298(sumAmount(getTotalRelief(boxRetriever)))
 }
