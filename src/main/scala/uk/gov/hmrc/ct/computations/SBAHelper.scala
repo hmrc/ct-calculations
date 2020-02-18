2e94a8561ee2cb218cad823c915aaa0939d7eccf
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2020-02-18 11:36:24 +0000

    [LG][DLS-1958] Rearranged boxes

--- src/main/scala/uk/gov/hmrc/ct/computations/SBAHelper.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/SBAHelper.scala
@@ -1,6 +1,17 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.ct.computations
@@ -17,8 +28,8 @@ trait SBAHelper  {
   val earliestWrittenContractId = "SBA01D"
   val nonResActivityId = "SBA01E"
   val costId = "SBA01F"
-  val filingPeriodQuestionId = "SBA01G"
-  val claimId = "SBA01H"
+  val claimId = "SBA01G"
+  val filingPeriodQuestionId = "SBA01H"
 
   val dateLowerBound = new LocalDate(2018, 10, 28)
   val exampleUpperBoundDate = new LocalDate(2019, 10, 28)
