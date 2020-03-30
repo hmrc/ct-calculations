b63fb7c3c57525200d8c4267f75efd499765eaad
Alok <alok.acharya@digital.hmrc.gov.uk>
2020-03-30 13:16:03 +0100

    DLS-2173: Added new validation for cp980 and cp982

--- src/main/scala/uk/gov/hmrc/ct/computations/CP981.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP981.scala
@@ -1,17 +1,6 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package uk.gov.hmrc.ct.computations
@@ -19,8 +8,14 @@ package uk.gov.hmrc.ct.computations
 import uk.gov.hmrc.ct.box._
 import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
-case class CP981(value: Option[Int]) extends CtBoxIdentifier(name = "PAYE and National insurance suffered from Off-payroll working (IR35)") with CtOptionalInteger with Input with ValidatableBox[ComputationsBoxRetriever] {
-  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = validateZeroOrPositiveInteger(this)
+case class CP981(value: Option[Int]) extends CtBoxIdentifier(name = "Deductions from Off-payroll working (IR35)") with CtOptionalInteger with Input with ValidatableBox[ComputationsBoxRetriever] {
+  override def validate(boxRetriever: ComputationsBoxRetriever ): Set[CtValidation] = {
+
+    validateZeroOrPositiveInteger(this)
+
+  }
+
+
 }
 
 object CP981 {
