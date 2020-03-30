a9fe48676286d217d61f7625d14780a4f464e5b8
Alok Acharya <31273756+AlokHMRC@users.noreply.github.com>
2020-03-30 15:31:11 +0100

    DLS-2173: Added new validation for cp980 and cp982 (#513)

--- src/main/scala/uk/gov/hmrc/ct/computations/CP982.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP982.scala
@@ -1,26 +1,29 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package uk.gov.hmrc.ct.computations
 
+import uk.gov.hmrc.ct.accounts.retriever.AccountsBoxRetriever
 import uk.gov.hmrc.ct.box._
 import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
-case class CP982(value: Option[Int]) extends CtBoxIdentifier(name = "Expenses from Off-payroll working (IR35)") with CtOptionalInteger with Input with ValidatableBox[ComputationsBoxRetriever] {
-  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = validateZeroOrPositiveInteger(this)
+case class CP982(value: Option[Int]) extends CtBoxIdentifier(name = "Expenses from Off-payroll working (IR35)") with CtOptionalInteger with Input with ValidatableBox[ComputationsBoxRetriever with AccountsBoxRetriever] {
+  override def validate(boxRetriever: ComputationsBoxRetriever with AccountsBoxRetriever): Set[CtValidation] = {
+    collectErrors(
+    validateZeroOrPositiveInteger(this),
+      cp981Breakdown(this, boxRetriever)
+    )
+
+  }
+
+
+  def cp981Breakdown(value: CtOptionalInteger, boxRetriever: ComputationsBoxRetriever with AccountsBoxRetriever): Set[CtValidation]= {
+    failIf(value.orZero > (boxRetriever.ac401().orZero - boxRetriever.ac403().orZero) - boxRetriever.cp980().orZero){
+      Set(CtValidation(Some("CP982"), "error.cp982.breakdown"))
+    }
+  }
 }
 
 object CP982 {
