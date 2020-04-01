4e5b8d970d05d7e133a37ab2927845dd3c9eb639
Callum Gibbons <>
2020-04-01 16:27:26 +0100

    DLS-2175 - Changed validation and calculations for OPW deduction boxes

--- src/main/scala/uk/gov/hmrc/ct/computations/CP983.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP983.scala
@@ -1,42 +1,16 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package uk.gov.hmrc.ct.computations
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, CtValidation, Input, ValidatableBox}
-import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
+import uk.gov.hmrc.ct.accounts.AC401
+import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtInteger, Linked}
 
-case class CP983(value: Option[Int]) extends CtBoxIdentifier(name = "Turnover from off-payroll working") with CtOptionalInteger with Input with ValidatableBox[ComputationsBoxRetriever]   {
-  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = {
-    val cp980 = boxRetriever.cp980()
-    val cp981 = boxRetriever.cp981()
-    val cp982 = boxRetriever.cp982()
+case class CP983(value: Int) extends CtBoxIdentifier(name = "Turnover from off-payroll working") with CtInteger
 
-      validateValueEqualToAdditions(cp980, cp981, cp982)
-  }
+object CP983 extends Linked[AC401, CP983] {
 
-  def validateValueEqualToAdditions(cp980: CP980, cp981: CP981, cp982: CP982): Set[CtValidation] = {
-    if(cp980.value.getOrElse(0) + cp981.value.getOrElse(0) + cp982.value.getOrElse(0) != value.getOrElse(0)){
-      Set(CtValidation(Some(id), s"error.$id.not.equalToAdditions"))
-    } else {
-      Set.empty
-    }
-  }
-}
-
-object CP983 {
-  def apply(int: Int): CP983 = CP983(Some(int))
+  override def apply(source: AC401): CP983 = CP983(source.orZero)
 }
--- src/main/scala/uk/gov/hmrc/ct/computations/formats/package.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/formats/package.scala
@@ -1,17 +1,6 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package uk.gov.hmrc.ct.computations
@@ -380,7 +369,7 @@ package object formats {
 
   implicit val CP982Format: Format[CP982] = new OptionalIntegerFormat[CP982](CP982.apply)
 
-  implicit val cp983Format: Format[CP983] = new OptionalIntegerFormat[CP983](CP983.apply)
+  implicit val cp983Format: Format[CP983] = new IntegerFormat[CP983](CP983.apply)
 
   implicit val cp997Format: Format[CP997] = new OptionalIntegerFormat[CP997](CP997.apply)
 
