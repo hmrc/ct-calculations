0a58d3d6b2f40fa4405cc0995e30d11d530ffec4
Alok Acharya <31273756+AlokHMRC@users.noreply.github.com>
2020-04-03 14:23:13 +0100

    Dls 2173 (#516)
    * DLS-2173: Added new validation for cp980 and cp982
    
    * DLS-2173: Linked AC403 and CP981

--- src/main/scala/uk/gov/hmrc/ct/computations/CP981.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP981.scala
@@ -1,21 +1,11 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package uk.gov.hmrc.ct.computations
 
+import uk.gov.hmrc.ct.accounts.AC403
 import uk.gov.hmrc.ct.box._
 import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
@@ -29,7 +19,6 @@ case class CP981(value: Option[Int]) extends CtBoxIdentifier(name = "Deductions 
 
 }
 
-object CP981 {
-
-  def apply(value: Int): CP981 = CP981(Some(value))
+object CP981 extends Linked[AC403, CP981] {
+  override def apply(source: AC403): CP981 = CP981(source.value)
 }
