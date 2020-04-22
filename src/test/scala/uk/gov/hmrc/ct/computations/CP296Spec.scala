29156285a6739a6bb86081e8ec6c5e55b711ab92
callum-gibbons <49552204+callum-gibbons@users.noreply.github.com>
2020-04-22 14:51:31 +0100

    DLS-2359 - Fixed issue with 771 not populating for start date same as AP start (#522)

---
+++ src/test/scala/uk/gov/hmrc/ct/computations/CP296Spec.scala
@@ -0,0 +1,45 @@
+package uk.gov.hmrc.ct.computations
+
+import org.joda.time.LocalDate
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatestplus.mockito.MockitoSugar
+import uk.gov.hmrc.ct.computations.stubs.StubbedComputationsBoxRetriever
+
+class CP296Spec extends WordSpec with Matchers with MockitoSugar {
+
+  "calculate" should {
+      val testBuildingCost = 1001
+      val testAPStart = new LocalDate("2019-04-01")
+
+    "not include buildings with start date before AP start date" in {
+      val testBuilding = Building(None, None, None, None, Some(testAPStart.minusDays(1)), None, Some(testBuildingCost), None)
+      val boxRetriever = new StubbedComputationsBoxRetriever {
+        override def cp1(): CP1 = CP1(testAPStart)
+        override def sba01(): SBA01 = SBA01(List(testBuilding))
+      }
+
+      CP296.calculate(boxRetriever).value shouldBe Some(0)
+    }
+
+    "include buildings with start date on AP start date" in {
+      val testBuilding = Building(None, None, None, None, Some(testAPStart), None, Some(testBuildingCost), None)
+      val boxRetriever = new StubbedComputationsBoxRetriever {
+        override def cp1(): CP1 = CP1(testAPStart)
+        override def sba01(): SBA01 = SBA01(List(testBuilding))
+      }
+
+      CP296.calculate(boxRetriever).value shouldBe Some(testBuildingCost)
+    }
+
+    "include buildings with start date after AP start date" in {
+      val testBuilding = Building(None, None, None, None, Some(testAPStart.plusDays(1)), None, Some(testBuildingCost), None)
+      val boxRetriever = new StubbedComputationsBoxRetriever {
+        override def cp1(): CP1 = CP1(testAPStart)
+        override def sba01(): SBA01 = SBA01(List(testBuilding))
+      }
+
+      CP296.calculate(boxRetriever).value shouldBe Some(testBuildingCost)
+    }
+  }
+
+}
