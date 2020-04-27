86d5ee61e28de891f033387ef76422d07f4dabcb
Callum Gibbons <49552204+callum-gibbons@users.noreply.github.com>
2020-04-27 10:59:35 +0100

    DLS-2347 - Change argument type on sbaApplies function

--- src/main/scala/uk/gov/hmrc/ct/computations/capitalAllowanceAndSBA/package.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/capitalAllowanceAndSBA/package.scala
@@ -6,13 +6,13 @@
 package uk.gov.hmrc.ct.computations
 
 import org.joda.time.LocalDate
-import uk.gov.hmrc.ct.box.{EndDate, StartDate}
+import uk.gov.hmrc.ct.box.CtDate
 
 package object capitalAllowanceAndSBA {
 
   val sbaApplies2019 = LocalDate.parse("2018-10-29")
 
-  def sbaApplies(apEndDate: StartDate): Boolean = apEndDate.value.isAfter(sbaApplies2019)
+  def sbaApplies(apDate: CtDate): Boolean = apDate.value.isAfter(sbaApplies2019)
 
 
 }
