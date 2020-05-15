cf35ddb301c29705acbb41809e5f5b44a66d391c
cavster <colm.m.cavanagh@gmail.com>
2020-05-15 11:48:16 +0100

    [CC,LG][DLS-1044]Fix for tradeing details journey not working when there is no trading profit

--- src/main/scala/uk/gov/hmrc/ct/computations/CP283a.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP283a.scala
@@ -1,17 +1,5 @@
 /*
  * Copyright 2020 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package uk.gov.hmrc.ct.computations
@@ -28,7 +16,7 @@ case class CP283a(value: Option[Int]) extends CtBoxIdentifier("Losses brought fo
 
   override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = {
     collectErrors(
-      requiredErrorIf(boxRetriever.cp281a().isPositive && !hasValue),
+      requiredErrorIf(boxRetriever.cp281a().isPositive && !hasValue && boxRetriever.cp117().isPositive),
       validateZeroOrPositiveInteger(this),
       sumOfBroughtForwardErrors(boxRetriever)
     )
--- src/test/scala/uk/gov/hmrc/ct/computations/CP283aSpec.scala
+++ src/test/scala/uk/gov/hmrc/ct/computations/CP283aSpec.scala
@@ -1,26 +1,15 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package uk.gov.hmrc.ct.computations
 
-import org.joda.time.LocalDate
 import org.mockito.Mockito.when
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{Matchers, WordSpec}
 import uk.gov.hmrc.ct.BoxValidationFixture
+import uk.gov.hmrc.ct.box.CtValidation
 import uk.gov.hmrc.ct.computations.Validators.LossesPreviousToCurrentFixture
 import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
@@ -38,12 +27,17 @@ class CP283aSpec extends WordSpec with Matchers with MockitoSugar with BoxValida
     makeBoxRetriever()
   }
 
-  private def makeBoxRetriever(cp281aValue: Option[Int] = Some(1)) = {
+  "when cp117 is zero, cp283a should pass when no value is entered" in {
+    setUpMocks()
+    CP283a(None).validate(makeBoxRetriever(None, 0)) shouldBe Set()
+  }
+
+  private def makeBoxRetriever(cp281aValue: Option[Int] = Some(1), cp117: Int = 1) = {
     val retriever = mock[ComputationsBoxRetriever]
     when(retriever.cp281a()).thenReturn(CP281a(cp281aValue))
     when(retriever.cp283a()).thenReturn(CP283a(None))
     when(retriever.cp283b()).thenReturn(CP283b(None))
-    when(retriever.cp117()).thenReturn(CP117(0))
+    when(retriever.cp117()).thenReturn(CP117(cp117))
     retriever
   }
 }
