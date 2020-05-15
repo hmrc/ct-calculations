758b10c8f6950b577507a3d26352a2ea1a12149c
Colm Cavanagh <colm.m.cavanagh@gmail.com>
2020-05-15 12:07:11 +0100

    [CC,LG][DLS-1044]Fix for tradeing details journey not working when there is no trading profit (#532)

--- src/test/scala/uk/gov/hmrc/ct/computations/CP283bSpec.scala
+++ src/test/scala/uk/gov/hmrc/ct/computations/CP283bSpec.scala
@@ -1,17 +1,5 @@
 /*
  * Copyright 2020 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package uk.gov.hmrc.ct.computations
@@ -37,7 +25,12 @@ class CP283bSpec extends WordSpec with Matchers with MockitoSugar with BoxValida
     makeBoxRetriever()
   }
 
-  private def makeBoxRetriever(cp281bValue: Option[Int] = Some(1)) = {
+  "when cp117 is zero, cp283b should pass when no value is entered" in {
+    setUpMocks()
+    CP283b(None).validate(makeBoxRetriever(None, 0)) shouldBe Set()
+  }
+
+  private def makeBoxRetriever(cp281bValue: Option[Int] = Some(1),cp117:Int = 1) = {
     val retriever = mock[ComputationsBoxRetriever]
     when(retriever.cp281b()).thenReturn(CP281b(cp281bValue))
     when(retriever.cp283a()).thenReturn(CP283a(None))
@@ -48,7 +41,7 @@ class CP283bSpec extends WordSpec with Matchers with MockitoSugar with BoxValida
     when(retriever.cp283d()).thenReturn(new CP283d(
       cp281bValue.map(i => Math.floor(i.toDouble / 2).toInt
       )))
-    when(retriever.cp117()).thenReturn(CP117(0))
+    when(retriever.cp117()).thenReturn(CP117(cp117))
     retriever
   }
 }
