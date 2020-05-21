4608f2194afe2fbc63231e24527f165248f95bea
Callum Gibbons <49552204+callum-gibbons@users.noreply.github.com>
2020-05-21 16:41:17 +0100

    Fixed filing unit tests

--- src/test/scala/uk/gov/hmrc/ct/accounts/frs105/boxes/AC415Spec.scala
+++ src/test/scala/uk/gov/hmrc/ct/accounts/frs105/boxes/AC415Spec.scala
@@ -5,10 +5,10 @@
 
 package uk.gov.hmrc.ct.accounts.frs105.boxes
 
-import uk.gov.hmrc.ct.accounts.frs105.retriever.Frs105AccountsBoxRetriever
+import uk.gov.hmrc.ct.accounts.retriever.AccountsBoxRetriever
 import uk.gov.hmrc.ct.accounts.{AccountsMoneyValidationFixture, MockFrs105AccountsRetriever}
 
-class AC415Spec extends AccountsMoneyValidationFixture[Frs105AccountsBoxRetriever] with MockFrs105AccountsRetriever {
+class AC415Spec extends AccountsMoneyValidationFixture[AccountsBoxRetriever] with MockFrs105AccountsRetriever {
 
   testAccountsMoneyValidation("AC415", AC415.apply)
 
