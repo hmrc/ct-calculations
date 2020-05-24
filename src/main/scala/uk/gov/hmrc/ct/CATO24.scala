5e537635d3d17e69c196eebcb5680d211b9c8ce1
Callum Gibbons <49552204+callum-gibbons@users.noreply.github.com>
2020-05-24 13:54:22 +0100

    DLS-2480 - Made CATO24 an input box

--- src/main/scala/uk/gov/hmrc/ct/CATO24.scala
+++ src/main/scala/uk/gov/hmrc/ct/CATO24.scala
@@ -5,6 +5,6 @@
 
 package uk.gov.hmrc.ct
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalBoolean}
+import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalBoolean, Input}
 
-case class CATO24(value: Option[Boolean]) extends CtBoxIdentifier(name = "Off payroll working") with CtOptionalBoolean
+case class CATO24(value: Option[Boolean]) extends CtBoxIdentifier(name = "Off payroll working") with CtOptionalBoolean with Input
