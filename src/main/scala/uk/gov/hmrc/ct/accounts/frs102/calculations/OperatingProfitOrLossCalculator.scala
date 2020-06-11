116f895d4266431e30d4096a381827af2ac69c47
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2020-06-11 12:30:10 +0100

    [LG][DLS-2601] Configured ac27 successfully

--- src/main/scala/uk/gov/hmrc/ct/accounts/frs102/calculations/OperatingProfitOrLossCalculator.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/frs102/calculations/OperatingProfitOrLossCalculator.scala
@@ -20,8 +20,8 @@ trait OperatingProfitOrLossCalculator extends DebitAwareCalculation {
   }
 
 
-  def calculateAC27(ac17: AC17, ac19: AC19, ac21: AC21, ac23: AC23 = AC23(None)): AC27 = {
-    sum(ac17, ac19, ac21, ac23)(AC27.apply)
+  def calculateAC27(ac25: AC25, ac19: AC19, ac21: AC21, ac23: AC23 = AC23(None)): AC27 = {
+    sum(ac25, ac19, ac21, ac23)(AC27.apply)
   }
 
   def calculateAC27OPW(ac16: AC16, ac18: AC18, ac20: AC20, ac401: AC401, ac22: AC22 = AC22(None)): AC26 = {
