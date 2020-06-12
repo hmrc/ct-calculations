f1cf618bf3382350ba4d5b132367218241410f70
Colm Cavanagh <colm.m.cavanagh@gmail.com>
2020-06-12 11:24:42 +0100

    [CC][DLS-2217]Reverted back until tina has Opw acp tests working (#543)

--- src/main/scala/uk/gov/hmrc/ct/computations/CP51.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP51.scala
@@ -20,17 +20,18 @@ import uk.gov.hmrc.ct.box._
 import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
 case class CP51(value: Option[Int]) extends CtBoxIdentifier(name = "Net loss on sale of fixed assets") with CtOptionalInteger with Input with ValidatableBox[ComputationsBoxRetriever] {
+  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = validateZeroOrPositiveInteger(this)
 
-  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = {
-    val cp33 = boxRetriever.cp33().value
+  //override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = {
+//  val cp33 = boxRetriever.cp33().value
 
-    (cp33) match {
+//  (cp33) match {
 
-      case Some(cp33Value) if (cp33Value < 0 && value.getOrElse(0)  < cp33Value) => Set(CtValidation(Some("CP51"), "error.CP51.must.be.less.then.CP33"))
-      case Some(cp33Value) if (cp33Value > 0 && value.getOrElse(0) != 0) => Set(CtValidation(Some("CP51"), "error.CP57.CP51.must.be.mutually.exclusive"))
-      case _ => validateZeroOrNegativeInteger(this)
-    }
-  }
+//    case Some(cp33Value) if (cp33Value < 0 && value.getOrElse(0)  < cp33Value) => Set(CtValidation(Some("CP51"), "error.CP51.must.be.less.then.CP33"))
+//    case Some(cp33Value) if (cp33Value > 0 && value.getOrElse(0) != 0) => Set(CtValidation(Some("CP51"), "error.CP57.CP51.must.be.mutually.exclusive"))
+//    case _ => validateZeroOrNegativeInteger(this)
+//  }
+//}
 }
 object CP51 {
 
--- src/test/scala/uk/gov/hmrc/ct/computations/CP51Spec.scala
+++ src/test/scala/uk/gov/hmrc/ct/computations/CP51Spec.scala
@@ -26,35 +26,35 @@ class CP51Spec  extends WordSpec with MockitoSugar with Matchers {
 
   val boxRetriever = mock[ComputationsBoxRetriever]
 
-  "CP51" should {
+//  "CP51" should {
 
-    " be valid if  cp 33 is NEGATIVE and someone enters in the same number " in {
-      when(boxRetriever.cp33()).thenReturn(CP33(Some(-10)))
-      CP51(Some(-10)).validate(boxRetriever) shouldBe Set()
-    }
+//    " be valid if  cp 33 is NEGATIVE and someone enters in the same number " in {
+//      when(boxRetriever.cp33()).thenReturn(CP33(Some(-10)))
+//      CP51(Some(-10)).validate(boxRetriever) shouldBe Set()
+//    }
 
-    " be valid if  cp 33 is NEGATIVE and someone enters in Zero " in {
-      when(boxRetriever.cp33()).thenReturn(CP33(Some(-10)))
-      CP51(Some(0)).validate(boxRetriever) shouldBe Set()
-    }
+//    " be valid if  cp 33 is NEGATIVE and someone enters in Zero " in {
+//      when(boxRetriever.cp33()).thenReturn(CP33(Some(-10)))
+//      CP51(Some(0)).validate(boxRetriever) shouldBe Set()
+//    }
 
-    " be valid if  cp 33 is NEGATIVE and someone enters in a greater negative number " in {
-      when(boxRetriever.cp33()).thenReturn(CP33(Some(-10)))
-      CP51(Some(-2)).validate(boxRetriever) shouldBe Set()
-    }
+//    " be valid if  cp 33 is NEGATIVE and someone enters in a greater negative number " in {
+//      when(boxRetriever.cp33()).thenReturn(CP33(Some(-10)))
+//      CP51(Some(-2)).validate(boxRetriever) shouldBe Set()
+//    }
 
-    " be in valid if someone enters in positive value  " in {
-      CP51(Some(10)).validate(boxRetriever) shouldBe Set(CtValidation(Some("CP51"),"error.CP51.mustBeNegativeOrZero",None))
-    }
+//    " be in valid if someone enters in positive value  " in {
+//      CP51(Some(10)).validate(boxRetriever) shouldBe Set(CtValidation(Some("CP51"),"error.CP51.mustBeNegativeOrZero",None))
+//    }
 
-    " be in valid if  cp 33 is positive and someone enters in value  " in {
-      when(boxRetriever.cp33()).thenReturn(CP33(Some(10)))
-      CP51(Some(1)).validate(boxRetriever) shouldBe Set(CtValidation(Some("CP51"), "error.CP57.CP51.must.be.mutually.exclusive"))
-    }
+//    " be in valid if  cp 33 is positive and someone enters in value  " in {
+//      when(boxRetriever.cp33()).thenReturn(CP33(Some(10)))
+//      CP51(Some(1)).validate(boxRetriever) shouldBe Set(CtValidation(Some("CP51"), "error.CP57.CP51.must.be.mutually.exclusive"))
+//    }
 
-    " be in valid if  cp 33 is negative and someone enters in value that is more negative " in {
-      when(boxRetriever.cp33()).thenReturn(CP33(Some(-10)))
-      CP51(Some(-11)).validate(boxRetriever) shouldBe Set(CtValidation(Some("CP51"), "error.CP51.must.be.less.then.CP33"))
-    }
-  }
+//    " be in valid if  cp 33 is negative and someone enters in value that is more negative " in {
+//      when(boxRetriever.cp33()).thenReturn(CP33(Some(-10)))
+//      CP51(Some(-11)).validate(boxRetriever) shouldBe Set(CtValidation(Some("CP51"), "error.CP51.must.be.less.then.CP33"))
+//    }
+//  }
 }
