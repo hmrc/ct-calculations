37d1efcc6faca1a008e9bdf0271ef4713d4b42bb
Alok <alok.acharya@digital.hmrc.gov.uk>
2020-06-22 15:59:52 +0100

    DLS-2298: Changed ap2 validation

--- src/main/scala/uk/gov/hmrc/ct/computations/AP2.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/AP2.scala
@@ -20,12 +20,8 @@ case class AP2(inputValue: Option[Int], defaultValue: Option[Int]) extends CtBox
   }
 
   private def totalErrors(boxRetriever: AccountsBoxRetriever with ComputationsBoxRetriever) = {
-    val ap2 = (inputValue, defaultValue) match {
-      case (iv, None) => iv
-      case (None, dv) => dv
-    }
 
-    failIf(boxRetriever.ap1() + ap2.getOrElse(0) + boxRetriever.ap3() != (boxRetriever.ac12().value.getOrElse(0) + boxRetriever.ac401().value.getOrElse(0))) {
+    failIf(boxRetriever.ap1() + boxRetriever.ap2().value.getOrElse(0) + boxRetriever.ap3() != (boxRetriever.ac12().value.getOrElse(0) + boxRetriever.ac401().value.getOrElse(0))) {
       Set(CtValidation(None, "error.apportionmentTurnover.total"))
     }
   }
