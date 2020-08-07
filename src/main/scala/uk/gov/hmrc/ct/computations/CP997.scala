14a729c551f8a780d6c4a84189d710d64a4b239e
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2020-08-07 15:19:07 +0100

    [LG][DLS_2018] wrote tests and made changes to cp997 logic

--- src/main/scala/uk/gov/hmrc/ct/computations/CP997.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP997.scala
@@ -1,17 +1,6 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package uk.gov.hmrc.ct.computations
@@ -42,20 +31,26 @@ case class CP997(value: Option[Int]) extends CP997Abstract(value)
   }
 
   private def cp997ExceedsNonTradingProfitAfterCPQ19(retriever: ComputationsBoxRetriever) = {
-    failIf(retriever.cp44() > 0 && retriever.cp44() < this.orZero){
+    failIf(retriever.cp44() > 0 && retriever.cp44() < this.orZero) {
       Set(CtValidation(Some("CP997"), "error.CP997.exceeds.nonTradingProfit"))
     }
   }
+
   private def lossesAlreadyUsed(retriever: ComputationsBoxRetriever) = {
-    failIf(retriever.cp281b().orZero - retriever.cp283b().orZero < this.orZero){
-      Set(CtValidation(Some("CP997"), "error.CP997.exceeds.leftLosses"))
+    val hasTradingProfit = retriever.cp117().isPositive
+
+    if (hasTradingProfit) {
+      failIf(retriever.cp281b().orZero - retriever.cp283b().orZero < this.orZero) {
+        Set(CtValidation(Some("CP997"), "error.CP997.exceeds.leftLosses.with.trading.profit"))
+      }
+    } else {
+      failIf(retriever.cp281b().orZero < this.orZero) {
+        Set(CtValidation(Some("CP997"), "error.CP997.exceeds.leftLosses.without.trading.profit"))
+      }
     }
   }
 }
 
 object CP997 {
-
   def apply(int: Int): CP997 = CP997(Some(int))
-
 }
-//CP997 saying “cannot exceed CP281b minus CP283b
