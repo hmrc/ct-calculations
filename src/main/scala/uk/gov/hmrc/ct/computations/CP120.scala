c78e4caa3919ebb124bcac02a4c034bbf48a0ee3
Callum Gibbons <49552204+callum-gibbons@users.noreply.github.com>
2020-12-10 15:21:36 +0000

    DLS-3668 - Changed validation for CP120 to only apply in eotho period

--- src/main/scala/uk/gov/hmrc/ct/computations/CP120.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP120.scala
@@ -20,5 +20,18 @@ import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalBoolean, CtValidation, Inp
 import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
 case class CP120(value: Option[Boolean]) extends CtBoxIdentifier(name = "Did you have Eat Out to Help Out Scheme") with CtOptionalBoolean with Input with ValidatableBox[ComputationsBoxRetriever] {
-  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = collectErrors(validateAsMandatory(this))
+  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = {
+    val apStartDate = boxRetriever.cp1().value
+    val apEndDate = boxRetriever.cp2().value
+
+    def validateAsMandatoryIfInDate() = {
+      if(covidSupport.doesPeriodCoverEotho(apStartDate, apEndDate)){
+        validateAsMandatory(this)()
+      } else {
+        validationSuccess
+      }
+    }
+
+    collectErrors(validateAsMandatoryIfInDate)
+  }
 }
