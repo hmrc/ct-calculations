ef80cf81d5c59dde89ff056b4322ba02c36a8c29
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2020-12-18 12:11:25 +0000

    [LG][DLS-3766] added CP105 and tests

--- src/main/scala/uk/gov/hmrc/ct/computations/machineryAndPlant/CP105.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/machineryAndPlant/CP105.scala
@@ -10,12 +10,13 @@ import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
 case class CP105(value: Int) extends CtBoxIdentifier(name = "Total expenditure qualifying for main rate") with CtInteger {}
 
-object CP105 extends Calculated[CP105, ComputationsBoxRetriever] {
+object CP105 extends Calculated[CP105, ComputationsBoxRetriever] with MachineryAndPlantCalculationsLogic {
   override def calculate(boxRetriever: ComputationsBoxRetriever): CP105 = {
     val carsQualifyingForMainRate = boxRetriever.cpAux2().value
     val mainRateExpenditureOtherThanCars = boxRetriever.cp82().value.getOrElse(0)
+    val total = sumOf(carsQualifyingForMainRate, mainRateExpenditureOtherThanCars)
 
-    CP105(carsQualifyingForMainRate + mainRateExpenditureOtherThanCars)
+    CP105(total)
   }
 }
 
