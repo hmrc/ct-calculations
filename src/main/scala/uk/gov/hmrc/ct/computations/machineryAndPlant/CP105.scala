6d6a865da04c2c377be60f7b75a8201642bfabc9
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2021-01-07 12:08:13 +0000

    [LG][DLS-3827] added cp110 and made alterations to other boxes

--- src/main/scala/uk/gov/hmrc/ct/computations/machineryAndPlant/CP105.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/machineryAndPlant/CP105.scala
@@ -8,14 +8,14 @@ package uk.gov.hmrc.ct.computations.machineryAndPlant
 import uk.gov.hmrc.ct.box.{Calculated, CtBoxIdentifier, CtInteger}
 import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
-case class CP105(value: Int) extends CtBoxIdentifier(name = "Total expenditure qualifying for main rate") with CtInteger {}
+case class CP105(value: Int) extends CtBoxIdentifier(name = "Total expenditure qualifying for main rate") with CtInteger
 
 object CP105 extends Calculated[CP105, ComputationsBoxRetriever] with MachineryAndPlantCalculationsLogic {
   override def calculate(boxRetriever: ComputationsBoxRetriever): CP105 = {
     val carsQualifyingForMainRate = boxRetriever.cpAux2().value
     val mainRateExpenditureOtherThanCars = boxRetriever.cp82().value.getOrElse(0)
-    val writtenDownValueBroughtForward=boxRetriever.cp78().value.getOrElse(0)
-    val total = sumOf(carsQualifyingForMainRate, mainRateExpenditureOtherThanCars,writtenDownValueBroughtForward)
+    val writtenDownValueBroughtForward=boxRetriever.cp78().value.getOrElse(0) // add 97
+    val total = sumOf(carsQualifyingForMainRate, mainRateExpenditureOtherThanCars, writtenDownValueBroughtForward)
 
     CP105(total)
   }
