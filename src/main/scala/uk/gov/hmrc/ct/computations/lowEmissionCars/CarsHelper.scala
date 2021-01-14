c22225ea7b102c5b2344d02be1273b0015691ad9
Callum Gibbons <49552204+callum-gibbons@users.noreply.github.com>
2021-01-14 12:00:29 +0000

    DLS-3352 - Added helper methods to group cars by pool

--- src/main/scala/uk/gov/hmrc/ct/computations/lowEmissionCars/CarsHelper.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/lowEmissionCars/CarsHelper.scala
@@ -5,6 +5,9 @@
 
 package uk.gov.hmrc.ct.computations.lowEmissionCars
 
+import uk.gov.hmrc.ct.computations.CP669.taxPoolForCar
+import uk.gov.hmrc.ct.computations.calculations.LowEmissionCarRate
+
 object CarsHelper {
 
   val lec01BoxId = "LEC01"
@@ -14,5 +17,10 @@ object CarsHelper {
   val emissionsId = "LEC01D"
   val dateOfPurchaseId = "LEC01E"
 
+  def assignCarPool(carsFromFilingState: List[AbstractLowEmissionCar]): Map[LowEmissionCarRate, List[AbstractLowEmissionCar]] =
+    carsFromFilingState.groupBy( car => taxPoolForCar(car))
 
+  def filterCarsByPool(carsGroupedByPool: Map[LowEmissionCarRate, List[AbstractLowEmissionCar]], rate: LowEmissionCarRate)= {
+    carsGroupedByPool.get(rate)
+  }
 }
