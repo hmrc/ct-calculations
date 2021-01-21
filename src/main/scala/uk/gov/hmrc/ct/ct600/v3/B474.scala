70269bcac2cf7a8bb93eb75169dedc7511361c0d
rachchan <25484244+rachchan@users.noreply.github.com>
2021-01-21 14:42:11 +0000

    Dls 3670 (#605)
    * new covid ct-600 boxes
    
    * updated boxes
    
    * fixed tests

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/B474.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/B474.scala
@@ -5,12 +5,13 @@
 
 package uk.gov.hmrc.ct.ct600.v3
 
-import uk.gov.hmrc.ct.box.{CtBigDecimal, CtBoxIdentifier, CtInteger, CtOptionalBigDecimal, Linked}
+
+import uk.gov.hmrc.ct.box.{CtBigDecimal, CtBoxIdentifier, CtInteger, CtOptionalBigDecimal, CtOptionalInteger, Linked}
 import uk.gov.hmrc.ct.computations.CP125
 
-case class B474(value: Option[BigDecimal]) extends CtBoxIdentifier("JRB and EOTHO overpayments") with CtOptionalBigDecimal
+case class B474(value: Option[Int]) extends CtBoxIdentifier("JRB and EOTHO overpayments") with CtOptionalInteger
 
 object B474 extends Linked[CP125, B474] {
 
-  override def apply(source: CP125): B474 = B474(source.value.map(value => BigDecimal(value)))
+  override def apply(source: CP125): B474 = B474(source.value)
 }
