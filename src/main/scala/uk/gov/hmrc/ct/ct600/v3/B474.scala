6c61f5e1a2c88792ebe49a79087d88e0a8640999
Rachel Chan <25484244+rachchan@users.noreply.github.com>
2021-01-21 15:29:38 +0000

    removed unused import

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/B474.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/B474.scala
@@ -5,7 +5,7 @@
 
 package uk.gov.hmrc.ct.ct600.v3
 
-import uk.gov.hmrc.ct.box.{CtBigDecimal, CtBoxIdentifier, CtInteger, CtOptionalBigDecimal, CtOptionalInteger, Linked}
+import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, Linked}
 import uk.gov.hmrc.ct.computations.CP125
 
 case class B474(value: Option[Int]) extends CtBoxIdentifier("JRB and EOTHO overpayments") with CtOptionalInteger
