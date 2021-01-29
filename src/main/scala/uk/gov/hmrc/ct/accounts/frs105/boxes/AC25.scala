23ef9a2b2d175dad356e12380b66e6875ff84b92
amarnathgujjalapudi <amarnath.gujjalapudi@digital.hmrc.gov.uk>
2021-01-29 12:47:37 +0000

    DLS-3832 Error message content is too long
    AC25 validation added

--- src/main/scala/uk/gov/hmrc/ct/accounts/frs105/boxes/AC25.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/frs105/boxes/AC25.scala
@@ -5,17 +5,37 @@
 
 package uk.gov.hmrc.ct.accounts.frs105.boxes
 
+import uk.gov.hmrc.ct.accounts.frs102.retriever.Frs102AccountsBoxRetriever
+import uk.gov.hmrc.ct.accounts.frs105.retriever.Frs105AccountsBoxRetriever
 import uk.gov.hmrc.ct.accounts.retriever.AccountsBoxRetriever
 import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.box.retriever.FilingAttributesBoxValueRetriever
+import uk.gov.hmrc.ct.validation.TurnoverValidation
 
 case class AC25(value: Option[Int]) extends CtBoxIdentifier(name = "Income from covid-19 business support grants")
   with CtOptionalInteger
   with Input
-  with ValidatableBox[AccountsBoxRetriever] {
+  with ValidatableBox[AccountsBoxRetriever with Frs102AccountsBoxRetriever with FilingAttributesBoxValueRetriever]
+  with TurnoverValidation {
 
-  override def validate(boxRetriever: AccountsBoxRetriever): Set[CtValidation] = {
+  val accountsStart = {
+    boxRetriever: AccountsBoxRetriever =>
+      boxRetriever.ac3()
+  }
+
+  val accountEnd = {
+    boxRetriever: AccountsBoxRetriever =>
+      boxRetriever.ac4()
+  }
+
+  override def validate(boxRetriever: AccountsBoxRetriever with Frs102AccountsBoxRetriever with FilingAttributesBoxValueRetriever): Set[CtValidation] = {
     collectErrors(
-      validateZeroOrPositiveInteger(this)
+      validateZeroOrPositiveInteger(this),
+      failIf(boxRetriever.hmrcFiling().value && !boxRetriever.abridgedFiling().value)(
+        collectErrors(
+          validateHmrcTurnover(boxRetriever, accountsStart, accountEnd, secondaryIncome = boxRetriever.ac13.orZero)
+        )
+      )
     )
   }
 }
