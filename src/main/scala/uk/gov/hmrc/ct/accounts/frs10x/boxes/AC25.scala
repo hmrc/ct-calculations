8768ca9c4a8e82e2d2b1b7e0cd4e983bc6fade5a
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2021-02-04 17:05:00 +0000

    [LG][DLS-3785b validation working on AC25

--- src/main/scala/uk/gov/hmrc/ct/accounts/frs10x/boxes/AC25.scala
+++ src/main/scala/uk/gov/hmrc/ct/accounts/frs10x/boxes/AC25.scala
@@ -7,14 +7,15 @@ package uk.gov.hmrc.ct.accounts.frs10x.boxes
 
 import uk.gov.hmrc.ct.accounts.frs10x.retriever.Frs10xAccountsBoxRetriever
 import uk.gov.hmrc.ct.accounts.retriever.AccountsBoxRetriever
-import uk.gov.hmrc.ct.accounts.{AC3, AC4}
+import uk.gov.hmrc.ct.accounts.{AC12, AC3, AC4, AccountsPreviousPeriodValidation}
 import uk.gov.hmrc.ct.box._
+import uk.gov.hmrc.ct.box.retriever.FilingAttributesBoxValueRetriever
 import uk.gov.hmrc.ct.validation.TurnoverValidation
 
 case class AC25(value: Option[Int]) extends CtBoxIdentifier(name = "Income from covid-19 business support grants")
   with CtOptionalInteger
   with Input
-  with ValidatableBox[Frs10xAccountsBoxRetriever] with TurnoverValidation {
+  with ValidatableBox[Frs10xAccountsBoxRetriever with FilingAttributesBoxValueRetriever] with TurnoverValidation {
 
   val accountsStart: AccountsBoxRetriever => AC3 = {
     boxRetriever: AccountsBoxRetriever =>
@@ -36,9 +37,39 @@ case class AC25(value: Option[Int]) extends CtBoxIdentifier(name = "Income from 
       boxRetriever.ac17()
   }
 
-  override def validate(boxRetriever: Frs10xAccountsBoxRetriever): Set[CtValidation] = {
+  override def validate(boxRetriever: Frs10xAccountsBoxRetriever with FilingAttributesBoxValueRetriever): Set[CtValidation] = {
+    val doCorrectValidation: Set[CtValidation] = {
+      if (value.getOrElse(0) == 0) {
+        validationSuccess
+      } else {
+        getCorrectBox(boxRetriever) match {
+          case box: AC13 => returnCorrectVali(boxRetriever, box, "AC13")
+          case box: AC17 => returnCorrectVali(boxRetriever, box, "AC17")
+        }
+      }
+    }
+
     collectErrors(
-      validateHmrcTurnover(boxRetriever, accountsStart, accountEnd, secondaryIncome = boxRetriever.ac17().orZero, minimumAmount = Some(0))
+      validateZeroOrPositiveInteger(this),
+      doCorrectValidation
     )
   }
+
+  private def getCorrectBox(boxRetriever: Frs10xAccountsBoxRetriever with FilingAttributesBoxValueRetriever): Box = {
+    val isAbridgedJourney = boxRetriever.abridgedFiling().value
+
+    val correctBoxWithBoxId: CtBoxIdentifier with CtOptionalInteger with Input with ValidatableBox[_ >: Frs10xAccountsBoxRetriever <: AccountsBoxRetriever] with AccountsPreviousPeriodValidation =
+      if (isAbridgedJourney) grossProfitOrLoss(boxRetriever)
+      else turnover(boxRetriever)
+
+    correctBoxWithBoxId
+  }
+
+
+  private def returnCorrectVali(boxRetriever: Frs10xAccountsBoxRetriever with FilingAttributesBoxValueRetriever, box: Box, boxId: String): Set[CtValidation] = {
+    validateHmrcTurnover(boxRetriever, accountsStart, accountEnd, errorSuffix = s".hmrc.turnover.$boxId", secondaryIncome = box.orZero, minimumAmount = Some(0))
+  }
+
+  private type Box = CtBoxIdentifier with CtOptionalInteger with Input with ValidatableBox[_ >: Frs10xAccountsBoxRetriever <: AccountsBoxRetriever] with AccountsPreviousPeriodValidation
+
 }
