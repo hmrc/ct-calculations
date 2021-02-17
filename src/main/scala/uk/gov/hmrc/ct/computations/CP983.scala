0cde82c0397fac8983de9c346eb3f205bc059614
amarnathgujjalapudi <amarnath.gujjalapudi@digital.hmrc.gov.uk>
2021-02-17 13:16:07 +0000

    DLS-3896 The user cannot continue the journey even they completed the page on the trading account page

--- src/main/scala/uk/gov/hmrc/ct/computations/CP983.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP983.scala
@@ -5,6 +5,7 @@
 
 package uk.gov.hmrc.ct.computations
 
+import uk.gov.hmrc.ct.accounts.frs10x.retriever.Frs10xDormancyBoxRetriever
 import uk.gov.hmrc.ct.box._
 import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 import uk.gov.hmrc.ct.validation.TurnoverValidation
@@ -12,16 +13,17 @@ import uk.gov.hmrc.ct.validation.TurnoverValidation
 case class CP983(value: Option[Int]) extends CtBoxIdentifier(name = "Turnover from off-payroll working")
   with CtOptionalInteger
   with Input
-  with ValidatableBox[ComputationsBoxRetriever]
+  with ValidatableBox[ComputationsBoxRetriever with Frs10xDormancyBoxRetriever]
   with TurnoverValidation {
 
   val compsStartDate = { br: ComputationsBoxRetriever => br.cp1() }
   val compsEndDate = { br: ComputationsBoxRetriever => br.cp2() }
 
-  override def validate(boxRetriever: ComputationsBoxRetriever): Set[CtValidation] = {
+  override def validate(boxRetriever: ComputationsBoxRetriever with Frs10xDormancyBoxRetriever): Set[CtValidation] = {
     val cato24 = boxRetriever.cato24
+    val dormant = boxRetriever.acq8999().orFalse
     collectErrors(
-      requiredErrorIf(cato24.isTrue && this.value.isEmpty),
+      requiredErrorIf(cato24.isTrue && !dormant &&  this.value.isEmpty),
       validateZeroOrPositiveInteger(this),
       validateHmrcTurnover(boxRetriever, compsStartDate, compsEndDate, errorSuffix = "", secondaryIncome = boxRetriever.cp7().orZero)
     )
