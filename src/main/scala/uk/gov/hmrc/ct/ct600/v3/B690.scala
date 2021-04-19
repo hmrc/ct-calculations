57e1eede038389adca108325b25766101db7cf60
rachchan <25484244+rachchan@users.noreply.github.com>
2021-04-19 12:08:42 +0100

    Update B690.scala

--- src/main/scala/uk/gov/hmrc/ct/ct600/v3/B690.scala
+++ src/main/scala/uk/gov/hmrc/ct/ct600/v3/B690.scala
@@ -1,7 +1,6 @@
 /*
  * Copyright 2021 HM Revenue & Customs
  *
- */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
 
 package uk.gov.hmrc.ct.ct600.v3
 
