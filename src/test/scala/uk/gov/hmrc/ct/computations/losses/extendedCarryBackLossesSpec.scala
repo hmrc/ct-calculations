9e0d66f93cabd8398fa8f83774ec4a654fcf5f7f
rachchan <25484244+rachchan@users.noreply.github.com>
2021-06-25 10:32:31 +0100

    touch up test (#649)

--- src/test/scala/uk/gov/hmrc/ct/computations/losses/extendedCarryBackLossesSpec.scala
+++ src/test/scala/uk/gov/hmrc/ct/computations/losses/extendedCarryBackLossesSpec.scala
@@ -7,7 +7,6 @@ import uk.gov.hmrc.ct.utils.UnitSpec
 class extendedCarryBackLossesSpec extends UnitSpec {
   "The Extended Carry Back Loss Period" should {
     "be false for end date before 01 April 2020" in {
-      val startDate = new LocalDate("2019-04-01")
       val endDate = new LocalDate("2020-03-31")
 
       val result = losses.doesPeriodCoverECBL(endDate)
@@ -15,7 +14,6 @@ class extendedCarryBackLossesSpec extends UnitSpec {
     }
 
     "be true for end date on 01 April 2020" in {
-      val startDate = new LocalDate("2019-03-31")
       val endDate = new LocalDate("2020-04-01")
 
       val result = losses.doesPeriodCoverECBL(endDate)
@@ -23,7 +21,6 @@ class extendedCarryBackLossesSpec extends UnitSpec {
     }
 
     "be true for period with end date landing between 01 April 2020 and 31 March 2022" in {
-      val startDate = new LocalDate("2021-01-01")
       val endDate = new LocalDate("2021-12-31")
 
       val result = losses.doesPeriodCoverECBL(endDate)
@@ -31,7 +28,6 @@ class extendedCarryBackLossesSpec extends UnitSpec {
     }
 
     "be false for end date after 31 March 2022" in {
-      val startDate = new LocalDate("2021-04-01")
       val endDate = new LocalDate("2022-04-01")
 
       val result = losses.doesPeriodCoverECBL(endDate)
