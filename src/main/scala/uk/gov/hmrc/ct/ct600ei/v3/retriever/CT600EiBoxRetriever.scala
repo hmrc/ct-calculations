0b6e7d6111e615b372cc701d460731d0ad1c98df
rachchan <25484244+rachchan@users.noreply.github.com>
2021-09-27 11:42:19 +0100

    validation (#654)
    * DLS-4519: Adding case classes for DIT
    
    * WIP started calculations for DIT001 and DIT002
    
    * ct600 working
    
    * fixed compilation headers
    
    * fixed test headers
    
    * DIT validation
    
    Co-authored-by: daglisood <88654936+daglisood@users.noreply.github.com>
    Co-authored-by: RGlossop <75027918+RGlossop@users.noreply.github.com>

---
+++ src/main/scala/uk/gov/hmrc/ct/ct600ei/v3/retriever/CT600EiBoxRetriever.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ct.ct600ei.v3.retriever
+
+
+import uk.gov.hmrc.ct.box.retriever.{BoxRetriever, FilingAttributesBoxValueRetriever}
+import uk.gov.hmrc.ct.ct600.v3.retriever.CT600BoxRetriever
+import uk.gov.hmrc.ct.ct600ei.v3.{DIT001, DIT002, DIT003}
+
+trait CT600EiBoxRetriever extends BoxRetriever {
+
+  self: CT600BoxRetriever =>
+
+  def dit001(): DIT001
+
+  def dit002(): DIT002
+
+  def dit003(): DIT003
+
+}
