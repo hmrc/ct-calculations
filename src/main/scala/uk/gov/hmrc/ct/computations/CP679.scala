7e085bfd6cd6b9eda6a40dc7ba44c5c3b2be5fca
GUJJALAPUDIA <56836634+GujjalapudiA@users.noreply.github.com>
2022-02-27 19:51:38 +0000

    DLS-4727
    Calculations added

--- src/main/scala/uk/gov/hmrc/ct/computations/CP679.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/CP679.scala
@@ -16,10 +16,11 @@
 
 package uk.gov.hmrc.ct.computations
 
-import uk.gov.hmrc.ct.box.{CtBoxIdentifier, CtOptionalInteger, SelfValidatableBox}
+import uk.gov.hmrc.ct.box.{Calculated, CtBoxIdentifier, CtOptionalInteger, SelfValidatableBox}
+import uk.gov.hmrc.ct.computations.calculations.NetSuperDeductionCalculator
 import uk.gov.hmrc.ct.computations.retriever.ComputationsBoxRetriever
 
-case class CP679(value: Option[Int]) extends CtBoxIdentifier(name = "net super deductions balancing charge")  with CtOptionalInteger with SelfValidatableBox[ComputationsBoxRetriever, Option[Int]] {
+case class CP679(value: Option[Int]) extends CtBoxIdentifier(name = "net super deductions claim")  with CtOptionalInteger with SelfValidatableBox[ComputationsBoxRetriever, Option[Int]] {
   override def validate(boxRetriever: ComputationsBoxRetriever) = {
     collectErrors(
       validateZeroOrPositiveInteger()
@@ -27,6 +28,10 @@ case class CP679(value: Option[Int]) extends CtBoxIdentifier(name = "net super d
   }
 }
 
-object CP679 {
-  def apply(value:Int): CP679 = CP679(Some(value))
+
+object CP679 extends Calculated[CP679, ComputationsBoxRetriever] with NetSuperDeductionCalculator {
+
+  override def calculate(fieldValueRetriever: ComputationsBoxRetriever): CP679 = {
+    netSuperDeductionClaim(fieldValueRetriever.cp677(),fieldValueRetriever.cp678)
+  }
 }
---
+++ src/main/scala/uk/gov/hmrc/ct/computations/calculations/NetSuperDeductionCalculator.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ct.computations.calculations
+
+import uk.gov.hmrc.ct.box.CtTypeConverters
+import uk.gov.hmrc.ct.computations._
+
+trait NetSuperDeductionCalculator extends CtTypeConverters {
+
+  def netSuperDeductionClaim(cp677: CP677, cp678: CP678): CP679 = {
+    if (cp677 > cp678)
+      CP679(Some(cp677 - cp678))
+    else
+      CP679(None)
+    }
+
+  def netSuperDeductionBalancingCharge(cp677: CP677, cp678: CP678): CP680 = {
+    if (cp677 < cp678)
+      CP680(Some(cp677 - cp678))
+    else
+      CP680(None)
+    }
+}
