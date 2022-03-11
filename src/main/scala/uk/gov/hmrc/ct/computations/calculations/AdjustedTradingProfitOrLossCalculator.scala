67b63466f0abb4adcaea6eb17f749c3dbb6b7fb8
Gaurav Sood <88654936+daglisood@users.noreply.github.com>
2022-03-11 14:39:49 +0000

    DLS-4824: Making changes after story update

--- src/main/scala/uk/gov/hmrc/ct/computations/calculations/AdjustedTradingProfitOrLossCalculator.scala
+++ src/main/scala/uk/gov/hmrc/ct/computations/calculations/AdjustedTradingProfitOrLossCalculator.scala
@@ -29,9 +29,9 @@ trait AdjustedTradingProfitOrLossCalculator extends CtTypeConverters {
                                      cp668: CP668,
                                      cp297: CP297,
                                      cpq19: CPQ19,
-                                     cp678: CP678,
-                                     cp679: CP679): CP117 = {
-    CP117(profit(cp44, cp54, cp59, cp186, cp91, cp670, cp668, cp297, cpq19, cp678, cp679) max 0)
+                                     cp679: CP679,
+                                     cp680: CP680): CP117 = {
+    CP117(profit(cp44, cp54, cp59, cp186, cp91, cp670, cp668, cp297, cpq19, cp679, cp680) max 0)
   }
 
   def calculateAdjustedTradingLoss(cp44: CP44,
@@ -43,9 +43,9 @@ trait AdjustedTradingProfitOrLossCalculator extends CtTypeConverters {
                                    cp668: CP668,
                                    cp297: CP297,
                                    cpq19: CPQ19,
-                                   cp678: CP678,
-                                   cp679: CP679): CP118 = {
-    CP118((profit(cp44, cp54, cp59, cp186, cp91, cp670, cp668, cp297, cpq19, cp678, cp679) min 0).abs)
+                                   cp679: CP679,
+                                   cp680: CP680): CP118 = {
+    CP118((profit(cp44, cp54, cp59, cp186, cp91, cp670, cp668, cp297, cpq19, cp679, cp680) min 0).abs)
   }
 
   private def profit(cp44: CP44,
@@ -57,11 +57,11 @@ trait AdjustedTradingProfitOrLossCalculator extends CtTypeConverters {
                      cp668: CP668,
                      cp297: CP297,
                      cpq19: CPQ19,
-                     cp678: CP678,
-                     cp679: CP679) = {
-    (cp678, cp679) match {
-      case (CP678(v), _) if v.nonEmpty => cp44 + cp54 - cp59 - cp186 + cp91 + cp670 - cp668 + cp678 - cp297
-      case (_, CP679(v)) if v.nonEmpty => cp44 + cp54 - cp59 - cp186 + cp91 + cp670 - cp668 - cp679 - cp297
+                     cp679: CP679,
+                     cp680:CP680) = {
+    (cp679, cp680) match {
+      case (CP679(v), _) if v.nonEmpty => cp44 + cp54 - cp59 - cp186 + cp91 + cp670 - cp668 - cp679 - cp297
+      case (_, CP680(v)) if v.nonEmpty => cp44 + cp54 - cp59 - cp186 + cp91 + cp670 - cp668 + cp680 - cp297
       case _ => cp44 + cp54 - cp59 - cp186 + cp91 + cp670 - cp668 - cp297
     }
 
